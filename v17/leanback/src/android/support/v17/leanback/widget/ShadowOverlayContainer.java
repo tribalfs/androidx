tribalfs/androidx

commit 4f34a05cdf73b68c3b2eb8678f740ab15225126a

Add rounded corners to leanback "cards".
b/16687089

Change-Id: If8997998304c5fd22823f2cf95677be90e35c136

v17/leanback/src/android/support/v17/leanback/widget/ShadowOverlayContainer.java
@@ -81,15 +81,18 @@ public class ShadowOverlayContainer extends ViewGroup {
     }
 
     /**
-     * Initialize shadows and/or color overlay.  Both are optional.
+     * Initialize shadows, color overlay, and rounded corners.  All are optional.
      */
-    public void initialize(boolean hasShadow, boolean hasColorDimOverlay) {
+    public void initialize(boolean hasShadow, boolean hasColorDimOverlay, boolean roundedCorners) {
         if (mInitialized) {
             throw new IllegalStateException();
         }
         mInitialized = true;
         if (hasShadow) {
-            mShadowImpl = ShadowHelper.getInstance().addShadow(this);
+            mShadowImpl = ShadowHelper.getInstance().addShadow(this, roundedCorners);
+        } else if (roundedCorners) {
+            RoundedRectHelper.getInstance().setRoundedRectBackground(this,
+                    android.graphics.Color.TRANSPARENT);
         }
         if (hasColorDimOverlay) {
             mColorDimOverlay = LayoutInflater.from(getContext())

v17/leanback/api21/android/support/v17/leanback/widget/RoundedRectHelperApi21.java
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package android.support.v17.leanback.widget;
+
+import android.support.v17.leanback.R;
+import android.graphics.Color;
+import android.graphics.Outline;
+import android.graphics.drawable.GradientDrawable;
+import android.view.ViewOutlineProvider;
+import android.view.View;
+
+class RoundedRectHelperApi21 {
+
+    private static int sCornerRadius;
+
+    private static final ViewOutlineProvider sOutlineProvider = new ViewOutlineProvider() {
+        @Override
+        public void getOutline(View view, Outline outline) {
+            if (sCornerRadius == 0) {
+                sCornerRadius = view.getResources().getDimensionPixelSize(
+                        R.dimen.lb_rounded_rect_corner_radius);
+            }
+            outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), sCornerRadius);
+            outline.setAlpha(1f);
+        }
+    };
+
+    public static void setRoundedRectBackground(View view, int color) {
+        if (color == Color.TRANSPARENT) {
+            view.setOutlineProvider(sOutlineProvider);
+        } else {
+            view.setOutlineProvider(ViewOutlineProvider.BACKGROUND);
+            view.setBackgroundResource(R.drawable.lb_rounded_rect_bg);
+            ((GradientDrawable) view.getBackground().mutate()).setColor(color);
+        }
+        view.setClipToOutline(true);
+    }
+
+    public static void clearBackground(View view) {
+        view.setBackground(null);
+        view.setOutlineProvider(ViewOutlineProvider.BACKGROUND);
+        view.setClipToOutline(false);
+    }
+}

v17/leanback/api21/android/support/v17/leanback/widget/ShadowHelperApi21.java
@@ -43,9 +43,14 @@ class ShadowHelperApi21 {
     }
 
     /* add shadows and return a implementation detail object */
-    public static Object addShadow(ViewGroup shadowContainer) {
+    public static Object addShadow(ViewGroup shadowContainer, boolean roundedCorners) {
         initializeResources(shadowContainer.getResources());
-        shadowContainer.setOutlineProvider(sOutlineProvider);
+        if (roundedCorners) {
+            RoundedRectHelperApi21.setRoundedRectBackground(shadowContainer,
+                    Color.TRANSPARENT);
+        } else {
+            shadowContainer.setOutlineProvider(sOutlineProvider);
+        }
         shadowContainer.setZ(sNormalZ);
         return shadowContainer;
     }

v17/leanback/res/drawable/lb_rounded_rect_bg.xml
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!--
+     Copyright (C) 2014 The Android Open Source Project
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+          http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+-->
+
+<shape xmlns:android="http://schemas.android.com/apk/res/android"
+    android:shape="rectangle">
+    <corners android:radius="@dimen/lb_rounded_rect_corner_radius" />
+</shape>

v17/leanback/res/values/dimens.xml
@@ -189,4 +189,6 @@
     <dimen name="lb_material_shadow_normal_z">2dp</dimen>
     <dimen name="lb_material_shadow_focused_z">8dp</dimen>
 
+    <dimen name="lb_rounded_rect_corner_radius">2dp</dimen>
+
 </resources>

v17/leanback/src/android/support/v17/leanback/widget/DetailsOverviewRowPresenter.java
@@ -224,11 +224,6 @@ public class DetailsOverviewRowPresenter extends RowPresenter {
         return mActionPresenterSelector.getOnActionClickedListener();
     }
 
-    private void applyBackground(View view) {
-        view.setBackgroundColor(mBackgroundColorSet ?
-                mBackgroundColor : getDefaultBackgroundColor(view.getContext()));
-    }
-
     /**
      * Sets the background color.  If not set, a default from the theme will be used.
      */
@@ -303,7 +298,7 @@ public class DetailsOverviewRowPresenter extends RowPresenter {
     }
 
     private void initDetailsOverview(ViewHolder vh) {
-        View overview = vh.view.findViewById(R.id.details_overview);
+        final View overview = vh.mOverviewView;
         ViewGroup.LayoutParams lp = overview.getLayoutParams();
         lp.height = getCardHeight(overview.getContext());
         overview.setLayoutParams(lp);
@@ -370,17 +365,21 @@ public class DetailsOverviewRowPresenter extends RowPresenter {
             }
         }
 
+        final int bgColor = mBackgroundColorSet ? mBackgroundColor :
+            getDefaultBackgroundColor(vh.mOverviewView.getContext());
+
         if (useMargin) {
             layoutParams.leftMargin = horizontalMargin;
             layoutParams.topMargin = layoutParams.bottomMargin = verticalMargin;
-            applyBackground(vh.mOverviewView);
+            RoundedRectHelper.getInstance().setRoundedRectBackground(vh.mOverviewView, bgColor);
             vh.mRightPanel.setBackground(null);
             vh.mImageView.setBackground(null);
         } else {
             layoutParams.leftMargin = layoutParams.topMargin = layoutParams.bottomMargin = 0;
-            applyBackground(vh.mRightPanel);
-            applyBackground(vh.mImageView);
-            vh.mOverviewView.setBackground(null);
+            vh.mRightPanel.setBackgroundColor(bgColor);
+            vh.mImageView.setBackgroundColor(bgColor);
+            RoundedRectHelper.getInstance().setRoundedRectBackground(vh.mOverviewView,
+                    Color.TRANSPARENT);
         }
         if (scaleImage) {
             vh.mImageView.setScaleType(ImageView.ScaleType.FIT_START);

v17/leanback/src/android/support/v17/leanback/widget/ListRowPresenter.java
@@ -86,6 +86,7 @@ public class ListRowPresenter extends RowPresenter {
     private int mZoomFactor;
     private boolean mShadowEnabled = true;
     private int mBrowseRowsFadingEdgeLength = -1;
+    private boolean mRoundedCornersEnabled = true;
 
     private static int sSelectedRowTopPadding;
     private static int sExpandedSelectedRowTopPadding;
@@ -162,7 +163,9 @@ public class ListRowPresenter extends RowPresenter {
             ShadowOverlayContainer wrapper = new ShadowOverlayContainer(root.getContext());
             wrapper.setLayoutParams(
                     new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-            wrapper.initialize(needsDefaultShadow(), needsDefaultListSelectEffect());
+            wrapper.initialize(needsDefaultShadow(),
+                    needsDefaultListSelectEffect(),
+                    areChildRoundedCornersEnabled());
             return wrapper;
         }
         @Override
@@ -175,7 +178,8 @@ public class ListRowPresenter extends RowPresenter {
     protected void initializeRowViewHolder(RowPresenter.ViewHolder holder) {
         super.initializeRowViewHolder(holder);
         final ViewHolder rowViewHolder = (ViewHolder) holder;
-        if (needsDefaultListSelectEffect() || needsDefaultShadow()) {
+        if (needsDefaultListSelectEffect() || needsDefaultShadow()
+                || areChildRoundedCornersEnabled()) {
             rowViewHolder.mItemBridgeAdapter.setWrapper(mCardWrapper);
         }
         if (needsDefaultListSelectEffect()) {
@@ -464,6 +468,21 @@ public class ListRowPresenter extends RowPresenter {
      */
     public final boolean getShadowEnabled() {
         return mShadowEnabled;
+    }
+
+    /**
+     * Enables or disabled rounded corners on children of this row.
+     * Supported on Android SDK >= L.
+     */
+    public final void enableChildRoundedCorners(boolean enable) {
+        mRoundedCornersEnabled = enable;
+    }
+
+    /**
+     * Returns true if rounded corners are enabled for children of this row.
+     */
+    public final boolean areChildRoundedCornersEnabled() {
+        return mRoundedCornersEnabled;
     }
 
     final boolean needsDefaultShadow() {

v17/leanback/src/android/support/v17/leanback/widget/PlaybackControlsRowPresenter.java
@@ -108,11 +108,11 @@ public class PlaybackControlsRowPresenter extends RowPresenter {
 
         void setBackground(View view) {
             if (mBgView != null) {
-                mBgView.setBackgroundColor(Color.TRANSPARENT);
+                RoundedRectHelper.getInstance().clearBackground(mBgView);
                 ShadowHelper.getInstance().clearZ(mBgView);
             }
             mBgView = view;
-            view.setBackgroundColor(mBackgroundColorSet ?
+            RoundedRectHelper.getInstance().setRoundedRectBackground(view, mBackgroundColorSet ?
                     mBackgroundColor : getDefaultBackgroundColor(view.getContext()));
             ShadowHelper.getInstance().setZ(view, 0f);
         }

commit bf19cbba26923cfd1da0e829f251b284b3ccac7a

Fix javadoc breakage from 4f34a05cdf73b68c3b2eb8678f740ab15225126a
Bug: 16687089
Change-Id: Ifb4424f5d9272887bdb0dcc5466a178a26c01c9d

v17/leanback/src/android/support/v17/leanback/widget/ShadowOverlayContainer.java
@@ -33,9 +33,9 @@ import android.graphics.Rect;
  * to parent.
  * </p>
  * <p>
- * {@link #initialize(boolean, boolean)} must be first called on the container to initialize
- * shadows and/or color overlay.  Then call {@link #wrap(View)} to insert wrapped view
- * into container.
+ * {@link #initialize(boolean, boolean, boolean)} must be first called on the container
+ * to initialize shadows and/or color overlay.  Then call {@link #wrap(View)} to insert
+ * wrapped view into container.
  * </p>
  * <p>
  * Call {@link #setShadowFocusLevel(float)} to control shadow alpha.
aosp-mirror/platform_frameworks_support

Enable USAGE_IO_OUTPUT and corresponding SetSurface and IOSend functions for in RS Compat lib.
The USAGE_IO support is provided by a shared lib (libRSSSupportIO)
dynamically loaded on devices running ICS or higher.

bug 12924018

Change-Id: Id9478f8db0d52dd6891e367dbef0d64a186ca399

v8/renderscript/jni/android_renderscript_RenderScript.cpp
@@ -24,7 +24,6 @@
 #include <android/bitmap.h>
 #include <android/log.h>
 #include "jni.h"
-#include <rs.h>
 #include <rsEnv.h>
 
 #include "rsDispatch.h"
@@ -93,8 +92,13 @@ private:
 
 // ---------------------------------------------------------------------------
 static dispatchTable dispatchTab;
+typedef void (*sAllocationIoSendFnPtr) (RsContext, RsAllocation, bool, dispatchTable);
+typedef void (*sAllocationSetSurfaceFnPtr) (JNIEnv *, jobject, RsContext, RsAllocation, RsNativeWindow, bool, dispatchTable);
+static sAllocationIoSendFnPtr sAllocationIoSend = NULL;
+static sAllocationSetSurfaceFnPtr sAllocationSetSurface = NULL;
+static bool mUseNative = false;
 
-static jboolean nLoadSO(JNIEnv *_env, jobject _this, jboolean useNative) {
+static jboolean nLoadSO(JNIEnv *_env, jobject _this, jboolean useNative, jboolean useIOlib) {
     void* handle = NULL;
     if (useNative) {
         handle = dlopen("libRS.so", RTLD_LAZY | RTLD_LOCAL);
@@ -105,12 +109,33 @@ static jboolean nLoadSO(JNIEnv *_env, jobject _this, jboolean useNative) {
         LOG_API("couldn't dlopen %s, %s", filename, dlerror());
         return false;
     }
-
+    
     if (loadSymbols(handle, dispatchTab) == false) {
         LOG_API("%s init failed!", filename);
         return false;
     }
     LOG_API("Successfully loaded %s", filename);
+
+    void* handleIO = NULL;
+    if(useNative || useIOlib) {
+        handleIO = dlopen("libRSSupportIO.so", RTLD_LAZY | RTLD_LOCAL);
+        if (handleIO == NULL) {
+            LOG_API("Couldn't load libRSSupportIO.so");
+            return false;
+        }
+        sAllocationIoSend = (sAllocationIoSendFnPtr)dlsym(handleIO, "AllocationIoSend");
+        if (sAllocationIoSend == NULL) {
+            LOG_API("Couldn't initialize sAllocationIoSend");
+            return false;
+        }
+        sAllocationSetSurface = (sAllocationSetSurfaceFnPtr)dlsym(handleIO, "AllocationSetSurface");
+        if (sAllocationSetSurface == NULL) {
+            LOG_API("Couldn't initialize sAllocationSetSurface");
+            return false;
+        }
+    }
+    
+    mUseNative = useNative;
     return true;
 }
 
@@ -356,6 +381,18 @@ nAllocationSyncAll(JNIEnv *_env, jobject _this, RsContext con, jint a, jint bits
 {
     LOG_API("nAllocationSyncAll, con(%p), a(%p), bits(0x%08x)", con, (RsAllocation)a, bits);
     dispatchTab.AllocationSyncAll(con, (RsAllocation)a, (RsAllocationUsageType)bits);
+}
+
+static void
+nAllocationSetSurface(JNIEnv *_env, jobject _this, RsContext con, jint alloc, jobject sur)
+{
+    sAllocationSetSurface(_env, _this, con, (RsAllocation)alloc, sur, mUseNative, dispatchTab);
+}
+
+static void
+nAllocationIoSend(JNIEnv *_env, jobject _this, RsContext con, jint alloc)
+{
+    sAllocationIoSend(con, (RsAllocation)alloc, mUseNative, dispatchTab);
 }
 
 static void
@@ -1017,7 +1054,7 @@ nSamplerCreate(JNIEnv *_env, jobject _this, RsContext con, jint magFilter, jint 
 static const char *classPathName = "android/support/v8/renderscript/RenderScript";
 
 static JNINativeMethod methods[] = {
-{"nLoadSO",                        "(Z)Z",                                    (bool*)nLoadSO },
+{"nLoadSO",                        "(ZZ)Z",                                   (bool*)nLoadSO },
 {"nDeviceCreate",                  "()I",                                     (void*)nDeviceCreate },
 {"nDeviceDestroy",                 "(I)V",                                    (void*)nDeviceDestroy },
 {"nDeviceSetConfig",               "(III)V",                                  (void*)nDeviceSetConfig },
@@ -1053,6 +1090,8 @@ static JNINativeMethod methods[] = {
 {"rsnAllocationCopyToBitmap",        "(IILandroid/graphics/Bitmap;)V",        (void*)nAllocationCopyToBitmap },
 
 {"rsnAllocationSyncAll",             "(III)V",                                (void*)nAllocationSyncAll },
+{"rsnAllocationSetSurface",          "(IILandroid/view/Surface;)V",           (void*)nAllocationSetSurface },
+{"rsnAllocationIoSend",              "(II)V",                                 (void*)nAllocationIoSend },
 {"rsnAllocationData1D",              "(IIIII[II)V",                           (void*)nAllocationData1D_i },
 {"rsnAllocationData1D",              "(IIIII[SI)V",                           (void*)nAllocationData1D_s },
 {"rsnAllocationData1D",              "(IIIII[BI)V",                           (void*)nAllocationData1D_b },

tribalfs/androidx

v8/renderscript/jni/android_renderscript_RenderScript.cpp
@@ -23,9 +23,8 @@
 #include <math.h>
 #include <android/bitmap.h>
 #include <android/log.h>
-#include "jni.h"
-#include <rsEnv.h>
 
+#include <rsEnv.h>
 #include "rsDispatch.h"
 #include <dlfcn.h>
 
@@ -113,6 +112,20 @@ static jboolean nLoadSO(JNIEnv *_env, jobject _this, jboolean useNative) {
     return true;
 }
 
+static ioSuppDT ioDispatch;
+static jboolean nLoadIOSO(JNIEnv *_env, jobject _this) {
+    void* handleIO = NULL;
+    handleIO = dlopen("libRSSupportIO.so", RTLD_LAZY | RTLD_LOCAL);
+    if (handleIO == NULL) {
+        LOG_API("Couldn't load libRSSupportIO.so");
+        return false;
+    }
+    if (loadIOSuppSyms(handleIO, ioDispatch) == false) {
+        LOG_API("libRSSupportIO init failed!");
+        return false;
+    }
+    return true;
+}
 // ---------------------------------------------------------------------------
 
 static void
@@ -355,6 +368,18 @@ nAllocationSyncAll(JNIEnv *_env, jobject _this, RsContext con, jint a, jint bits
 {
     LOG_API("nAllocationSyncAll, con(%p), a(%p), bits(0x%08x)", con, (RsAllocation)a, bits);
     dispatchTab.AllocationSyncAll(con, (RsAllocation)a, (RsAllocationUsageType)bits);
+}
+
+static void
+nAllocationSetSurface(JNIEnv *_env, jobject _this, RsContext con, jint alloc, jobject sur)
+{
+    ioDispatch.sAllocationSetSurface(_env, _this, con, (RsAllocation)alloc, sur, dispatchTab);
+}
+
+static void
+nAllocationIoSend(JNIEnv *_env, jobject _this, RsContext con, jint alloc)
+{
+    dispatchTab.AllocationIoSend(con, (RsAllocation)alloc);
 }
 
 static void
@@ -1016,7 +1041,8 @@ nSamplerCreate(JNIEnv *_env, jobject _this, RsContext con, jint magFilter, jint 
 static const char *classPathName = "android/support/v8/renderscript/RenderScript";
 
 static JNINativeMethod methods[] = {
-{"nLoadSO",                        "(Z)Z",                                    (bool*)nLoadSO },
+{"nLoadSO",                        "(Z)Z",                                   (bool*)nLoadSO },
+{"nLoadIOSO",                      "()Z",                                     (bool*)nLoadIOSO },
 {"nDeviceCreate",                  "()I",                                     (void*)nDeviceCreate },
 {"nDeviceDestroy",                 "(I)V",                                    (void*)nDeviceDestroy },
 {"nDeviceSetConfig",               "(III)V",                                  (void*)nDeviceSetConfig },
@@ -1052,6 +1078,8 @@ static JNINativeMethod methods[] = {
 {"rsnAllocationCopyToBitmap",        "(IILandroid/graphics/Bitmap;)V",        (void*)nAllocationCopyToBitmap },
 
 {"rsnAllocationSyncAll",             "(III)V",                                (void*)nAllocationSyncAll },
+{"rsnAllocationSetSurface",          "(IILandroid/view/Surface;)V",           (void*)nAllocationSetSurface },
+{"rsnAllocationIoSend",              "(II)V",                                 (void*)nAllocationIoSend },
 {"rsnAllocationData1D",              "(IIIII[II)V",                           (void*)nAllocationData1D_i },
 {"rsnAllocationData1D",              "(IIIII[SI)V",                           (void*)nAllocationData1D_s },
 {"rsnAllocationData1D",              "(IIIII[BI)V",                           (void*)nAllocationData1D_b },
aosp-mirror/platform_frameworks_support

Allow clicks on the custom widget in CheckBoxPreference
Most of this was copied verbatim from SwitchPreferenceCompat.

When using a custom widgetLayout, a listener needs to be attached to
listen for changes to the checked state, so that this can be passed
into the preference system.

Change-Id: Ieabdfd76d82f8bfd7f313613e25766cea8c3b65b

v7/preference/src/android/support/v7/preference/CheckBoxPreference.java
@@ -22,7 +22,9 @@ import android.support.v4.content.res.TypedArrayUtils;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
+import android.widget.CheckBox;
 import android.widget.Checkable;
+import android.widget.CompoundButton;
 
 /**
  * A {@link Preference} that provides checkbox widget
@@ -35,6 +37,20 @@ import android.widget.Checkable;
  * @attr ref android.R.styleable#CheckBoxPreference_disableDependentsState
  */
 public class CheckBoxPreference extends TwoStatePreference {
+    private final Listener mListener = new Listener();
+
+    private class Listener implements CompoundButton.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (!callChangeListener(isChecked)) {
+                // Listener didn't like it, change it back.
+                // CompoundButton will make sure we don't recurse.
+                buttonView.setChecked(!isChecked);
+                return;
+            }
+            CheckBoxPreference.this.setChecked(isChecked);
+        }
+    }
 
     public CheckBoxPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         this(context, attrs, defStyleAttr, 0);
@@ -104,8 +120,14 @@ public class CheckBoxPreference extends TwoStatePreference {
     }
 
     private void syncCheckboxView(View view) {
+        if (view instanceof CheckBox) {
+            ((CheckBox) view).setOnCheckedChangeListener(null);
+        }
         if (view instanceof Checkable) {
             ((Checkable) view).setChecked(mChecked);
+        }
+        if (view instanceof CheckBox) {
+            ((CheckBox) view).setOnCheckedChangeListener(mListener);
         }
     }
 }

tribalfs/androidx

v7/preference/src/android/support/v7/preference/CheckBoxPreference.java
@@ -23,6 +23,7 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.accessibility.AccessibilityManager;
 import android.widget.Checkable;
+import android.widget.CompoundButton;
 
 /**
  * A {@link Preference} that provides checkbox widget
@@ -35,6 +36,20 @@ import android.widget.Checkable;
  * @attr ref android.R.styleable#CheckBoxPreference_disableDependentsState
  */
 public class CheckBoxPreference extends TwoStatePreference {
+    private final Listener mListener = new Listener();
+
+    private class Listener implements CompoundButton.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if (!callChangeListener(isChecked)) {
+                // Listener didn't like it, change it back.
+                // CompoundButton will make sure we don't recurse.
+                buttonView.setChecked(!isChecked);
+                return;
+            }
+            CheckBoxPreference.this.setChecked(isChecked);
+        }
+    }
 
     public CheckBoxPreference(Context context, AttributeSet attrs, int defStyleAttr) {
         this(context, attrs, defStyleAttr, 0);
@@ -73,10 +88,7 @@ public class CheckBoxPreference extends TwoStatePreference {
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
 
-        View checkboxView = holder.findViewById(android.R.id.checkbox);
-        if (checkboxView != null && checkboxView instanceof Checkable) {
-            ((Checkable) checkboxView).setChecked(mChecked);
-        }
+        syncCheckboxView(holder.findViewById(android.R.id.checkbox));
 
         syncSummaryView(holder);
     }
@@ -105,8 +117,14 @@ public class CheckBoxPreference extends TwoStatePreference {
     }
 
     private void syncCheckboxView(View view) {
+        if (view instanceof CompoundButton) {
+            ((CompoundButton) view).setOnCheckedChangeListener(null);
+        }
         if (view instanceof Checkable) {
             ((Checkable) view).setChecked(mChecked);
+        }
+        if (view instanceof CompoundButton) {
+            ((CompoundButton) view).setOnCheckedChangeListener(mListener);
         }
     }
 }
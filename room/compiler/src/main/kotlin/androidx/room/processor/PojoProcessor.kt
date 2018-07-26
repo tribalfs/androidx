aosp-mirror/platform_frameworks_support

Entity fields can be ignored by names
Fields inherited from an Entity parent can be ignored by adding
ignoredColumns to @Entity.

Bug: 63522075
Test: EntityProcessorTest, PojoProcessorTest
Change-Id: Ie737babc43e47cea383ee7c697c9b1fd8d8e5253

room/compiler/src/main/kotlin/androidx/room/processor/PojoProcessor.kt
@@ -82,6 +82,7 @@ class PojoProcessor private constructor(
     val bindingScope: FieldProcessor.BindingScope,
     val parent: EmbeddedField?,
     val referenceStack: LinkedHashSet<Name> = LinkedHashSet(),
+    val ignoredColumns: List<String>,
     private val delegate: Delegate
 ) : KotlinMetadataProcessor {
     val context = baseContext.fork(element)
@@ -108,7 +109,8 @@ class PojoProcessor private constructor(
             element: TypeElement,
             bindingScope: FieldProcessor.BindingScope,
             parent: EmbeddedField?,
-            referenceStack: LinkedHashSet<Name> = LinkedHashSet()
+            referenceStack: LinkedHashSet<Name> = LinkedHashSet(),
+            ignoredColumns: List<String> = emptyList()
         ): PojoProcessor {
             val (pojoElement, delegate) = if (element.hasAnnotation(AutoValue::class)) {
                 val elementUtils = context.processingEnv.elementUtils
@@ -127,6 +129,7 @@ class PojoProcessor private constructor(
                     bindingScope = bindingScope,
                     parent = parent,
                     referenceStack = referenceStack,
+                    ignoredColumns = ignoredColumns,
                     delegate = delegate)
         }
     }
@@ -188,7 +191,15 @@ class PojoProcessor private constructor(
                         ?: emptyList()
 
         val subFields = embeddedFields.flatMap { it.pojo.fields }
-        val fields = myFields + subFields
+        val combinedFields = myFields + subFields
+        val fields = combinedFields.filterNot { ignoredColumns.contains(it.columnName) }
+        val missingIgnoredColumns = ignoredColumns.filterNot { ignoredColumn ->
+            combinedFields.any { it.columnName == ignoredColumn }
+        }
+        context.checker.check(
+                missingIgnoredColumns.isEmpty(), element,
+                ProcessorErrors.missingIgnoredColumns(missingIgnoredColumns)
+        )
 
         val myRelationsList = allFields[Relation::class]
                 ?.mapNotNull {

room/compiler/src/test/kotlin/androidx/room/processor/PojoProcessorTest.kt
@@ -1012,6 +1012,70 @@ class PojoProcessorTest {
         }.compilesWithoutError().withWarningCount(0)
     }
 
+    @Test
+    fun ignoredColumns() {
+        simpleRun(
+                """
+                package foo.bar;
+                import androidx.room.*;
+                public class ${MY_POJO.simpleName()} {
+                    public String foo;
+                    public String bar;
+                }
+                """.toJFO(MY_POJO.toString())) { invocation ->
+            val pojo = PojoProcessor.createFor(context = invocation.context,
+                    element = invocation.typeElement(MY_POJO.toString()),
+                    bindingScope = FieldProcessor.BindingScope.READ_FROM_CURSOR,
+                    ignoredColumns = listOf("bar"),
+                    parent = null).process()
+            assertThat(pojo.fields.find { it.name == "foo" }, notNullValue())
+            assertThat(pojo.fields.find { it.name == "bar" }, nullValue())
+        }.compilesWithoutError()
+    }
+
+    @Test
+    fun ignoredColumns_columnInfo() {
+        simpleRun(
+                """
+                package foo.bar;
+                import androidx.room.*;
+                public class ${MY_POJO.simpleName()} {
+                    public String foo;
+                    @ColumnInfo(name = "my_bar")
+                    public String bar;
+                }
+                """.toJFO(MY_POJO.toString())) { invocation ->
+            val pojo = PojoProcessor.createFor(context = invocation.context,
+                    element = invocation.typeElement(MY_POJO.toString()),
+                    bindingScope = FieldProcessor.BindingScope.READ_FROM_CURSOR,
+                    ignoredColumns = listOf("my_bar"),
+                    parent = null).process()
+            assertThat(pojo.fields.find { it.name == "foo" }, notNullValue())
+            assertThat(pojo.fields.find { it.name == "bar" }, nullValue())
+        }.compilesWithoutError()
+    }
+
+    @Test
+    fun ignoredColumns_missing() {
+        simpleRun(
+                """
+                package foo.bar;
+                import androidx.room.*;
+                public class ${MY_POJO.simpleName()} {
+                    public String foo;
+                    public String bar;
+                }
+                """.toJFO(MY_POJO.toString())) { invocation ->
+            val pojo = PojoProcessor.createFor(context = invocation.context,
+                    element = invocation.typeElement(MY_POJO.toString()),
+                    bindingScope = FieldProcessor.BindingScope.READ_FROM_CURSOR,
+                    ignoredColumns = listOf("no_such_column"),
+                    parent = null).process()
+            assertThat(pojo.fields.find { it.name == "foo" }, notNullValue())
+            assertThat(pojo.fields.find { it.name == "bar" }, notNullValue())
+        }.failsToCompile().withErrorContaining("Non-existent columns")
+    }
+
     private fun singleRun(
         code: String,
         vararg jfos: JavaFileObject,

tribalfs/androidx

room/compiler/src/main/kotlin/androidx/room/processor/PojoProcessor.kt
@@ -82,6 +82,7 @@ class PojoProcessor private constructor(
     val bindingScope: FieldProcessor.BindingScope,
     val parent: EmbeddedField?,
     val referenceStack: LinkedHashSet<Name> = LinkedHashSet(),
+    val ignoredColumns: Set<String>,
     private val delegate: Delegate
 ) : KotlinMetadataProcessor {
     val context = baseContext.fork(element)
@@ -108,7 +109,8 @@ class PojoProcessor private constructor(
             element: TypeElement,
             bindingScope: FieldProcessor.BindingScope,
             parent: EmbeddedField?,
-            referenceStack: LinkedHashSet<Name> = LinkedHashSet()
+            referenceStack: LinkedHashSet<Name> = LinkedHashSet(),
+            ignoredColumns: Set<String> = emptySet()
         ): PojoProcessor {
             val (pojoElement, delegate) = if (element.hasAnnotation(AutoValue::class)) {
                 val elementUtils = context.processingEnv.elementUtils
@@ -127,6 +129,7 @@ class PojoProcessor private constructor(
                     bindingScope = bindingScope,
                     parent = parent,
                     referenceStack = referenceStack,
+                    ignoredColumns = ignoredColumns,
                     delegate = delegate)
         }
     }
@@ -188,7 +191,15 @@ class PojoProcessor private constructor(
                         ?: emptyList()
 
         val subFields = embeddedFields.flatMap { it.pojo.fields }
-        val fields = myFields + subFields
+        val combinedFields = myFields + subFields
+        val fields = combinedFields.filterNot { ignoredColumns.contains(it.columnName) }
+        val missingIgnoredColumns = ignoredColumns.filterNot { ignoredColumn ->
+            combinedFields.any { it.columnName == ignoredColumn }
+        }
+        context.checker.check(
+                missingIgnoredColumns.isEmpty(), element,
+                ProcessorErrors.missingIgnoredColumns(missingIgnoredColumns)
+        )
 
         val myRelationsList = allFields[Relation::class]
                 ?.mapNotNull {

room/compiler/src/test/kotlin/androidx/room/processor/PojoProcessorTest.kt
@@ -1012,6 +1012,71 @@ class PojoProcessorTest {
         }.compilesWithoutError().withWarningCount(0)
     }
 
+    @Test
+    fun ignoredColumns() {
+        simpleRun(
+                """
+                package foo.bar;
+                import androidx.room.*;
+                public class ${MY_POJO.simpleName()} {
+                    public String foo;
+                    public String bar;
+                }
+                """.toJFO(MY_POJO.toString())) { invocation ->
+            val pojo = PojoProcessor.createFor(context = invocation.context,
+                    element = invocation.typeElement(MY_POJO.toString()),
+                    bindingScope = FieldProcessor.BindingScope.READ_FROM_CURSOR,
+                    ignoredColumns = setOf("bar"),
+                    parent = null).process()
+            assertThat(pojo.fields.find { it.name == "foo" }, notNullValue())
+            assertThat(pojo.fields.find { it.name == "bar" }, nullValue())
+        }.compilesWithoutError()
+    }
+
+    @Test
+    fun ignoredColumns_columnInfo() {
+        simpleRun(
+                """
+                package foo.bar;
+                import androidx.room.*;
+                public class ${MY_POJO.simpleName()} {
+                    public String foo;
+                    @ColumnInfo(name = "my_bar")
+                    public String bar;
+                }
+                """.toJFO(MY_POJO.toString())) { invocation ->
+            val pojo = PojoProcessor.createFor(context = invocation.context,
+                    element = invocation.typeElement(MY_POJO.toString()),
+                    bindingScope = FieldProcessor.BindingScope.READ_FROM_CURSOR,
+                    ignoredColumns = setOf("my_bar"),
+                    parent = null).process()
+            assertThat(pojo.fields.find { it.name == "foo" }, notNullValue())
+            assertThat(pojo.fields.find { it.name == "bar" }, nullValue())
+        }.compilesWithoutError()
+    }
+
+    @Test
+    fun ignoredColumns_missing() {
+        simpleRun(
+                """
+                package foo.bar;
+                import androidx.room.*;
+                public class ${MY_POJO.simpleName()} {
+                    public String foo;
+                    public String bar;
+                }
+                """.toJFO(MY_POJO.toString())) { invocation ->
+            val pojo = PojoProcessor.createFor(context = invocation.context,
+                    element = invocation.typeElement(MY_POJO.toString()),
+                    bindingScope = FieldProcessor.BindingScope.READ_FROM_CURSOR,
+                    ignoredColumns = setOf("no_such_column"),
+                    parent = null).process()
+            assertThat(pojo.fields.find { it.name == "foo" }, notNullValue())
+            assertThat(pojo.fields.find { it.name == "bar" }, notNullValue())
+        }.failsToCompile().withErrorContaining(
+                ProcessorErrors.missingIgnoredColumns(listOf("no_such_column")))
+    }
+
     private fun singleRun(
         code: String,
         vararg jfos: JavaFileObject,
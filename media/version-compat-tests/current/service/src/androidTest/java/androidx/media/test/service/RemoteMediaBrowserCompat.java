aosp-mirror/platform_frameworks_support

Introduce RemoteMediaBrowserCompat(Service)
To test calls from remote MediaBrowserCompat, this CL introduces a
new class RemoteMediaBrowserCompat.

Bug: 79558823
Test: Passed RemoteMediaBrowserCompatTest
Change-Id: Ice72046f79a7f5215dbd5ce42bde421d81c4b1df

media/version-compat-tests/current/service/src/androidTest/java/androidx/media/test/service/RemoteMediaBrowserCompat.java
@@ -1,0 +1,270 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media.test.service;
+
+import static androidx.media.test.lib.CommonConstants.ACTION_MEDIA_BROWSER_COMPAT;
+import static androidx.media.test.lib.CommonConstants.REMOTE_MEDIA_BROWSER_COMPAT_SERVICE;
+import static androidx.media.test.lib.TestUtils.WAIT_TIME_MS;
+
+import static junit.framework.TestCase.fail;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.mediacompat.testlib.IRemoteMediaBrowserCompat;
+import android.support.v4.media.MediaBrowserCompat;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.media.test.lib.MediaBrowserCompatConstants;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Represents remote {@link MediaBrowserCompat} the client app's MediaBrowserCompatService.
+ * Users can run {@link MediaBrowserCompat} methods remotely with this object.
+ */
+public class RemoteMediaBrowserCompat {
+    private static final String TAG = "RemoteMediaBrowserCompat";
+
+    private final String mBrowserId;
+    private final Context mContext;
+    private final CountDownLatch mCountDownLatch;
+
+    private ServiceConnection mServiceConnection;
+    private IRemoteMediaBrowserCompat mBinder;
+
+    /**
+     * Create a {@link MediaBrowserCompat} in the client app.
+     * Should NOT be called main thread.
+     */
+    public RemoteMediaBrowserCompat(Context context, ComponentName serviceComponent) {
+        mContext = context;
+        mBrowserId = UUID.randomUUID().toString();
+        mCountDownLatch = new CountDownLatch(1);
+        mServiceConnection = new MyServiceConnection();
+        if (!connectToService()) {
+            fail("Failed to connect to the RemoteMediaBrowserCompatService.");
+        }
+        create(serviceComponent);
+    }
+
+    public void cleanUp() {
+        disconnect();
+        disconnectFromService();
+    }
+
+    /**
+     * Run a test-specific custom command the service app.
+     *
+     * @param command Pre-defined command code.
+     *                One of the constants {@link MediaBrowserCompatConstants}.
+     * @param args A {@link Bundle} which contains pre-defined arguments.
+     */
+    public void runCustomTestCommands(int command, @Nullable Bundle args) {
+        try {
+            mBinder.runCustomTestCommands(mBrowserId, command, args);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call runCustomTestCommands(). command=" + command
+                    + " , args=" + args);
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // MediaBrowserCompat methods
+    ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Connect to the given media browser service.
+     *
+     * @param waitForConnection true if the remote browser needs to wait for the connection,
+     *                          false otherwise.
+     */
+    public void connect(boolean waitForConnection) {
+        try {
+            mBinder.connect(mBrowserId, waitForConnection);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call connect()");
+        }
+    }
+
+    public void disconnect() {
+        try {
+            mBinder.disconnect(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call disconnect()");
+        }
+    }
+
+    public boolean isConnected() {
+        try {
+            return mBinder.isConnected(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call isConnected()");
+            return false;
+        }
+    }
+
+    public ComponentName getServiceComponent() {
+        try {
+            return mBinder.getServiceComponent(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getServiceComponent()");
+            return null;
+        }
+    }
+
+    public String getRoot() {
+        try {
+            return mBinder.getRoot(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getRoot()");
+            return null;
+        }
+    }
+
+    public Bundle getExtras() {
+        try {
+            return mBinder.getExtras(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getExtras()");
+            return null;
+        }
+    }
+
+    public Bundle getSessionToken() {
+        try {
+            return mBinder.getSessionToken(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getSessionToken()");
+            return null;
+        }
+    }
+
+    public void subscribe(String parentId, Bundle options) {
+        try {
+            mBinder.subscribe(mBrowserId, parentId, options);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call subscribe()");
+        }
+    }
+
+    public void unsubscribe(String parentId) {
+        try {
+            mBinder.unsubscribe(mBrowserId, parentId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call unsubscribe()");
+        }
+    }
+
+    public void getItem(String mediaId) {
+        try {
+            mBinder.getItem(mBrowserId, mediaId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getItem()");
+        }
+    }
+
+    public void search(String query, Bundle extras) {
+        try {
+            mBinder.search(mBrowserId, query, extras);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call search()");
+        }
+    }
+
+    public void sendCustomAction(String action, Bundle extras) {
+        try {
+            mBinder.sendCustomAction(mBrowserId, action, extras);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call sendCustomAction()");
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Non-public methods
+    ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Connects to client app's RemoteMediaBrowserCompatService.
+     * Should NOT be called main thread.
+     *
+     * @return true if connected successfully, false if failed to connect.
+     */
+    private boolean connectToService() {
+        final Intent intent = new Intent(ACTION_MEDIA_BROWSER_COMPAT);
+        intent.setComponent(REMOTE_MEDIA_BROWSER_COMPAT_SERVICE);
+
+        boolean bound = false;
+        try {
+            bound = mContext.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+        } catch (Exception ex) {
+            Log.e(TAG, "Failed to bind to the RemoteMediaBrowserCompatService.");
+        }
+
+        if (bound) {
+            try {
+                mCountDownLatch.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException ex) {
+                Log.e(TAG, "InterruptedException while waiting for onServiceConnected.", ex);
+            }
+        }
+        return mBinder != null;
+    }
+
+    /**
+     * Disconnects from client app's RemoteMediaBrowserCompatService.
+     */
+    private void disconnectFromService() {
+        if (mServiceConnection != null) {
+            mContext.unbindService(mServiceConnection);
+            mServiceConnection = null;
+        }
+    }
+
+    /**
+     * Create a {@link MediaBrowserCompat} in the client app.
+     * Should be used after successful connection through {@link #connectToService()}.
+     */
+    private void create(ComponentName serviceComponent) {
+        try {
+            mBinder.create(mBrowserId, serviceComponent);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to create default browser with given serviceComponent.");
+        }
+    }
+
+    class MyServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "Connected to client app's RemoteMediaBrowserCompatService.");
+            mBinder = IRemoteMediaBrowserCompat.Stub.asInterface(service);
+            mCountDownLatch.countDown();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "Disconnected from client app's RemoteMediaBrowserCompatService.");
+        }
+    }
+}

media/version-compat-tests/lib/src/main/java/androidx/media/test/lib/CommonConstants.java
@@ -33,6 +33,9 @@ public class CommonConstants {
     public static final ComponentName REMOTE_MEDIA_CONTROLLER_COMPAT_SERVICE = new ComponentName(
             IntentUtil.CLIENT_PACKAGE_NAME,
             "androidx.media.test.client.RemoteMediaControllerCompatService");
+    public static final ComponentName REMOTE_MEDIA_BROWSER_COMPAT_SERVICE = new ComponentName(
+            IntentUtil.CLIENT_PACKAGE_NAME,
+            "androidx.media.test.client.RemoteMediaBrowserCompatService");
     public static final ComponentName MOCK_MEDIA_SESSION_SERVICE = new ComponentName(
             IntentUtil.SERVICE_PACKAGE_NAME,
             "androidx.media.test.service.MockMediaSessionService2");
@@ -47,6 +50,8 @@ public class CommonConstants {
             "androidx.media.test.action.MEDIA_SESSION_COMPAT";
     public static final String ACTION_MEDIA_CONTROLLER_COMPAT =
             "androidx.media.test.action.MEDIA_CONTROLLER_COMPAT";
+    public static final String ACTION_MEDIA_BROWSER_COMPAT =
+            "androidx.media.test.action.MEDIA_BROWSER_COMPAT";
 
     // Keys for arguments.
     public static final String KEY_STREAM = "stream";

tribalfs/androidx

media/version-compat-tests/current/service/src/androidTest/java/androidx/media/test/service/RemoteMediaBrowserCompat.java
@@ -1,0 +1,251 @@
+/*
+ * Copyright 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.media.test.service;
+
+import static androidx.media.test.lib.CommonConstants.ACTION_MEDIA_BROWSER_COMPAT;
+import static androidx.media.test.lib.CommonConstants.MEDIA_BROWSER_COMPAT_PROVIDER_SERVICE;
+import static androidx.media.test.lib.TestUtils.WAIT_TIME_MS;
+
+import static junit.framework.TestCase.fail;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.mediacompat.testlib.IRemoteMediaBrowserCompat;
+import android.support.v4.media.MediaBrowserCompat;
+import android.util.Log;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Represents remote {@link MediaBrowserCompat} the client app's MediaBrowserCompatProviderService.
+ * Users can run {@link MediaBrowserCompat} methods remotely with this object.
+ */
+public class RemoteMediaBrowserCompat {
+    private static final String TAG = "RemoteMediaBrowserCompat";
+
+    private final String mBrowserId;
+    private final Context mContext;
+    private final CountDownLatch mCountDownLatch;
+
+    private ServiceConnection mServiceConnection;
+    private IRemoteMediaBrowserCompat mBinder;
+
+    /**
+     * Create a {@link MediaBrowserCompat} in the client app.
+     * Should NOT be called main thread.
+     */
+    public RemoteMediaBrowserCompat(Context context, ComponentName serviceComponent) {
+        mContext = context;
+        mBrowserId = UUID.randomUUID().toString();
+        mCountDownLatch = new CountDownLatch(1);
+        mServiceConnection = new MyServiceConnection();
+        if (!connectToService()) {
+            fail("Failed to connect to the MediaBrowserCompatProviderService.");
+        }
+        create(serviceComponent);
+    }
+
+    public void cleanUp() {
+        disconnect();
+        disconnectFromService();
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // MediaBrowserCompat methods
+    ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Connect to the given media browser service.
+     *
+     * @param waitForConnection true if the remote browser needs to wait for the connection,
+     *                          false otherwise.
+     */
+    public void connect(boolean waitForConnection) {
+        try {
+            mBinder.connect(mBrowserId, waitForConnection);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call connect()");
+        }
+    }
+
+    public void disconnect() {
+        try {
+            mBinder.disconnect(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call disconnect()");
+        }
+    }
+
+    public boolean isConnected() {
+        try {
+            return mBinder.isConnected(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call isConnected()");
+            return false;
+        }
+    }
+
+    public ComponentName getServiceComponent() {
+        try {
+            return mBinder.getServiceComponent(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getServiceComponent()");
+            return null;
+        }
+    }
+
+    public String getRoot() {
+        try {
+            return mBinder.getRoot(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getRoot()");
+            return null;
+        }
+    }
+
+    public Bundle getExtras() {
+        try {
+            return mBinder.getExtras(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getExtras()");
+            return null;
+        }
+    }
+
+    public Bundle getSessionToken() {
+        try {
+            return mBinder.getSessionToken(mBrowserId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getSessionToken()");
+            return null;
+        }
+    }
+
+    public void subscribe(String parentId, Bundle options) {
+        try {
+            mBinder.subscribe(mBrowserId, parentId, options);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call subscribe()");
+        }
+    }
+
+    public void unsubscribe(String parentId) {
+        try {
+            mBinder.unsubscribe(mBrowserId, parentId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call unsubscribe()");
+        }
+    }
+
+    public void getItem(String mediaId) {
+        try {
+            mBinder.getItem(mBrowserId, mediaId);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call getItem()");
+        }
+    }
+
+    public void search(String query, Bundle extras) {
+        try {
+            mBinder.search(mBrowserId, query, extras);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call search()");
+        }
+    }
+
+    public void sendCustomAction(String action, Bundle extras) {
+        try {
+            mBinder.sendCustomAction(mBrowserId, action, extras);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to call sendCustomAction()");
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Non-public methods
+    ////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Connects to client app's MediaBrowserCompatProviderService.
+     * Should NOT be called main thread.
+     *
+     * @return true if connected successfully, false if failed to connect.
+     */
+    private boolean connectToService() {
+        final Intent intent = new Intent(ACTION_MEDIA_BROWSER_COMPAT);
+        intent.setComponent(MEDIA_BROWSER_COMPAT_PROVIDER_SERVICE);
+
+        boolean bound = false;
+        try {
+            bound = mContext.bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+        } catch (Exception ex) {
+            Log.e(TAG, "Failed to bind to the MediaBrowserCompatProviderService.");
+        }
+
+        if (bound) {
+            try {
+                mCountDownLatch.await(WAIT_TIME_MS, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException ex) {
+                Log.e(TAG, "InterruptedException while waiting for onServiceConnected.", ex);
+            }
+        }
+        return mBinder != null;
+    }
+
+    /**
+     * Disconnects from client app's MediaBrowserCompatProviderService.
+     */
+    private void disconnectFromService() {
+        if (mServiceConnection != null) {
+            mContext.unbindService(mServiceConnection);
+            mServiceConnection = null;
+        }
+    }
+
+    /**
+     * Create a {@link MediaBrowserCompat} in the client app.
+     * Should be used after successful connection through {@link #connectToService()}.
+     */
+    private void create(ComponentName serviceComponent) {
+        try {
+            mBinder.create(mBrowserId, serviceComponent);
+        } catch (RemoteException ex) {
+            Log.e(TAG, "Failed to create default browser with given serviceComponent.");
+        }
+    }
+
+    class MyServiceConnection implements ServiceConnection {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "Connected to client app's MediaBrowserCompatProviderService.");
+            mBinder = IRemoteMediaBrowserCompat.Stub.asInterface(service);
+            mCountDownLatch.countDown();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "Disconnected from client app's MediaBrowserCompatProviderService.");
+        }
+    }
+}

media/version-compat-tests/lib/src/main/java/androidx/media/test/lib/CommonConstants.java
@@ -21,18 +21,22 @@ import android.support.mediacompat.testlib.util.IntentUtil;
 
 public class CommonConstants {
 
-    public static final ComponentName REMOTE_MEDIA_SESSION2_SERVICE = new ComponentName(
+    public static final ComponentName MEDIA_SESSION2_PROVIDER_SERVICE = new ComponentName(
             IntentUtil.SERVICE_PACKAGE_NAME,
-            "androidx.media.test.service.RemoteMediaSession2Service");
-    public static final ComponentName REMOTE_MEDIA_CONTROLLER2_SERVICE = new ComponentName(
+            "androidx.media.test.service.MediaSession2ProviderService");
+    public static final ComponentName MEDIA_CONTROLLER2_PROVIDER_SERVICE = new ComponentName(
             IntentUtil.CLIENT_PACKAGE_NAME,
-            "androidx.media.test.client.RemoteMediaController2Service");
-    public static final ComponentName REMOTE_MEDIA_SESSION_COMPAT_SERVICE = new ComponentName(
+            "androidx.media.test.client.MediaController2ProviderService");
+    public static final ComponentName MEDIA_SESSION_COMPAT_PROVIDER_SERVICE = new ComponentName(
             IntentUtil.SERVICE_PACKAGE_NAME,
-            "androidx.media.test.service.RemoteMediaSessionCompatService");
-    public static final ComponentName REMOTE_MEDIA_CONTROLLER_COMPAT_SERVICE = new ComponentName(
+            "androidx.media.test.service.MediaSessionCompatProviderService");
+    public static final ComponentName MEDIA_CONTROLLER_COMPAT_PROVIDER_SERVICE = new ComponentName(
             IntentUtil.CLIENT_PACKAGE_NAME,
-            "androidx.media.test.client.RemoteMediaControllerCompatService");
+            "androidx.media.test.client.MediaControllerCompatProviderService");
+    public static final ComponentName MEDIA_BROWSER_COMPAT_PROVIDER_SERVICE = new ComponentName(
+            IntentUtil.CLIENT_PACKAGE_NAME,
+            "androidx.media.test.client.MediaBrowserCompatProviderService");
+
     public static final ComponentName MOCK_MEDIA_LIBRARY_SERVICE = new ComponentName(
             IntentUtil.SERVICE_PACKAGE_NAME,
             "androidx.media.test.service.MockMediaLibraryService2");
@@ -44,6 +48,8 @@ public class CommonConstants {
             "androidx.media.test.action.MEDIA_SESSION_COMPAT";
     public static final String ACTION_MEDIA_CONTROLLER_COMPAT =
             "androidx.media.test.action.MEDIA_CONTROLLER_COMPAT";
+    public static final String ACTION_MEDIA_BROWSER_COMPAT =
+            "androidx.media.test.action.MEDIA_BROWSER_COMPAT";
 
     // Keys for arguments.
     public static final String KEY_STREAM = "stream";
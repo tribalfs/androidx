aosp-mirror/platform_frameworks_support

Initial support for PagedList loading state
Bug: 73246446
Bug: 112714476
Tests: ./gradlew :paging:paging-common:test

Add error/retry capability to PagedList start/end. Can be observed via
Adapter/Differ.

Still TODO:
 - refresh error/retry. Requires coordination with observables.
 - TiledPagedList state support

Change-Id: I4f6e19301775b6973fbae0b9668954d37f61ab5e

paging/common/src/main/java/androidx/paging/DataSource.java
@@ -17,6 +17,7 @@
 package androidx.paging;
 
 import androidx.annotation.AnyThread;
+import androidx.annotation.GuardedBy;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
@@ -277,6 +278,8 @@ public abstract class DataSource<Key, Value> {
         // mSignalLock protects mPostExecutor, and mHasSignalled
         private final Object mSignalLock = new Object();
         private Executor mPostExecutor = null;
+
+        @GuardedBy("mSignalLock")
         private boolean mHasSignalled = false;
 
         LoadCallbackHelper(@NonNull DataSource dataSource, @PageResult.ResultType int resultType,
@@ -298,6 +301,7 @@ public abstract class DataSource<Key, Value> {
          *
          * @return true if DataSource was invalid, and invalid result dispatched
          */
+        @SuppressWarnings("BooleanMethodIsAlwaysInverted")
         boolean dispatchInvalidResultIfInvalid() {
             if (mDataSource.isInvalid()) {
                 dispatchResultToReceiver(PageResult.<T>getInvalidResult());
@@ -306,12 +310,21 @@ public abstract class DataSource<Key, Value> {
             return false;
         }
 
-        void dispatchResultToReceiver(final @NonNull PageResult<T> result) {
+        void dispatchResultToReceiver(@NonNull PageResult<T> result) {
+            dispatchToReceiver(result, null, false);
+        }
+
+        void dispatchErrorToReceiver(@NonNull Throwable error, boolean isRetryable) {
+            dispatchToReceiver(null, error, isRetryable);
+        }
+
+        private void dispatchToReceiver(final @Nullable PageResult<T> result,
+                final @Nullable Throwable error, final boolean isRetryable) {
             Executor executor;
             synchronized (mSignalLock) {
                 if (mHasSignalled) {
                     throw new IllegalStateException(
-                            "callback.onResult already called, cannot call again.");
+                            "callback.onResult/onError already called, cannot call again.");
                 }
                 mHasSignalled = true;
                 executor = mPostExecutor;
@@ -321,11 +334,21 @@ public abstract class DataSource<Key, Value> {
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
-                        mReceiver.onPageResult(mResultType, result);
+                        dispatchOnCurrentThread(result, error, isRetryable);
                     }
                 });
             } else {
+                dispatchOnCurrentThread(result, error, isRetryable);
+            }
+        }
+
+        @SuppressWarnings("ConstantConditions")
+        void dispatchOnCurrentThread(@Nullable PageResult<T> result,
+                @Nullable Throwable error, boolean isRetryable) {
+            if (result != null) {
                 mReceiver.onPageResult(mResultType, result);
+            } else {
+                mReceiver.onPageError(mResultType, error, isRetryable);
             }
         }
     }

paging/common/src/main/java/androidx/paging/TiledPagedList.java
@@ -79,6 +79,11 @@ class TiledPagedList<T> extends PagedList<T>
                 deferBoundaryCallbacks(deferEmpty, deferBegin, deferEnd);
             }
         }
+
+        @Override
+        public void onPageError(int type, @NonNull Throwable throwable, boolean retryable) {
+            throw new IllegalStateException("Tiled error handling not yet implemented");
+        }
     };
 
     @WorkerThread

tribalfs/androidx

Test: ./gradlew paging:paging-runtime:cC paging:paging-common:test
 - NW + DB extension for DataSources (using new error/retry)

paging/common/src/main/java/androidx/paging/DataSource.java
@@ -17,6 +17,7 @@
 package androidx.paging;
 
 import androidx.annotation.AnyThread;
+import androidx.annotation.GuardedBy;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
@@ -277,6 +278,8 @@ public abstract class DataSource<Key, Value> {
         // mSignalLock protects mPostExecutor, and mHasSignalled
         private final Object mSignalLock = new Object();
         private Executor mPostExecutor = null;
+
+        @GuardedBy("mSignalLock")
         private boolean mHasSignalled = false;
 
         LoadCallbackHelper(@NonNull DataSource dataSource, @PageResult.ResultType int resultType,
@@ -298,6 +301,7 @@ public abstract class DataSource<Key, Value> {
          *
          * @return true if DataSource was invalid, and invalid result dispatched
          */
+        @SuppressWarnings("BooleanMethodIsAlwaysInverted")
         boolean dispatchInvalidResultIfInvalid() {
             if (mDataSource.isInvalid()) {
                 dispatchResultToReceiver(PageResult.<T>getInvalidResult());
@@ -306,12 +310,21 @@ public abstract class DataSource<Key, Value> {
             return false;
         }
 
-        void dispatchResultToReceiver(final @NonNull PageResult<T> result) {
+        void dispatchResultToReceiver(@NonNull PageResult<T> result) {
+            dispatchToReceiver(result, null, false);
+        }
+
+        void dispatchErrorToReceiver(@NonNull Throwable error, boolean retryable) {
+            dispatchToReceiver(null, error, retryable);
+        }
+
+        private void dispatchToReceiver(final @Nullable PageResult<T> result,
+                final @Nullable Throwable error, final boolean retryable) {
             Executor executor;
             synchronized (mSignalLock) {
                 if (mHasSignalled) {
                     throw new IllegalStateException(
-                            "callback.onResult already called, cannot call again.");
+                            "callback.onResult/onError already called, cannot call again.");
                 }
                 mHasSignalled = true;
                 executor = mPostExecutor;
@@ -321,11 +334,21 @@ public abstract class DataSource<Key, Value> {
                 executor.execute(new Runnable() {
                     @Override
                     public void run() {
-                        mReceiver.onPageResult(mResultType, result);
+                        dispatchOnCurrentThread(result, error, retryable);
                     }
                 });
             } else {
+                dispatchOnCurrentThread(result, error, retryable);
+            }
+        }
+
+        @SuppressWarnings("ConstantConditions")
+        void dispatchOnCurrentThread(@Nullable PageResult<T> result,
+                @Nullable Throwable error, boolean retryable) {
+            if (result != null) {
                 mReceiver.onPageResult(mResultType, result);
+            } else {
+                mReceiver.onPageError(mResultType, error, retryable);
             }
         }
     }

paging/common/src/main/java/androidx/paging/TiledPagedList.java
@@ -79,6 +79,11 @@ class TiledPagedList<T> extends PagedList<T>
                 deferBoundaryCallbacks(deferEmpty, deferBegin, deferEnd);
             }
         }
+
+        @Override
+        public void onPageError(int type, @NonNull Throwable error, boolean retryable) {
+            throw new IllegalStateException("Tiled error handling not yet implemented");
+        }
     };
 
     @WorkerThread
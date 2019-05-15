aosp-mirror/platform_frameworks_support

Adding Level 2 Semantic API to Playground/Demo app
Bug: 132197447
Test: Ran the app locally
Change-Id: Ie5ca603d601fce6325e560c0b17b22afc784edd1

ui/framework/integration-tests/framework-demos/src/main/java/androidx/ui/framework/demos/SemanticsL2.kt
@@ -1,0 +1,73 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.framework.demos
+
+import androidx.compose.Children
+import androidx.compose.Composable
+import androidx.compose.composer
+
+/**
+ * This is a level 3 API, where the user uses the [SemanticActionBuilder] to build the action.
+ * This component provides default values for all the parameters to the builder, the developer has
+ * to just supply the callback lambda.
+ */
+@Suppress("FunctionName")
+@Composable
+fun Properties(
+    label: String = "",
+    visibility: Visibility = Visibility.Undefined,
+    actions: Set<SemanticAction<out Any?>> = setOf(),
+    @Children children: @Composable() () -> Unit
+) {
+    val propertySet = mutableSetOf<SemanticProperty<out Any>>()
+
+    if (!label.isEmpty()) {
+        propertySet.add(Label(label))
+    }
+
+    if (visibility != Visibility.Undefined) {
+        propertySet.add(visibility)
+    }
+
+    Semantics(properties = propertySet, actions = actions) { children() }
+}
+
+/**
+ * This is a component that emits a semantic node with a single action.
+ *
+ * Since this example does not have node merging implemented, we just creates an semantic action
+ * using the supplied parameters and then invokes the supplied lambda.
+ *
+ * Action(params) { semanticAction->
+ * ...
+ * }
+ *
+ * For now the [Properties] component accepts a set of actions, but once this is finally
+ * implemented, we will merge the nodes automatically.
+ */
+@Suppress("FunctionName")
+@Composable
+fun <T> Action(
+    phrase: String = "",
+    defaultParam: T,
+    types: Set<ActionType> = setOf(),
+    action: (ActionParam<T>) -> Unit,
+    @Children block: @Composable() (SemanticAction<T>) -> Unit
+) {
+    val semanticAction = SemanticAction<T>(phrase, defaultParam, types, action)
+    block.invoke(semanticAction)
+}

tribalfs/androidx

ui/framework/integration-tests/framework-demos/src/main/java/androidx/ui/framework/demos/SemanticsL2.kt
@@ -1,0 +1,71 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.ui.framework.demos
+
+import androidx.compose.Children
+import androidx.compose.Composable
+import androidx.compose.composer
+
+/**
+ * This is a level 2 API. This component makes it easier to find/discover available properties.
+ */
+@Suppress("FunctionName")
+@Composable
+fun SemanticProperties(
+    label: String = "",
+    visibility: Visibility = Visibility.Undefined,
+    actions: Set<SemanticAction<out Any?>> = setOf(),
+    @Children children: @Composable() () -> Unit
+) {
+    val propertySet = mutableSetOf<SemanticProperty<out Any>>()
+
+    if (!label.isEmpty()) {
+        propertySet.add(Label(label))
+    }
+
+    if (visibility != Visibility.Undefined) {
+        propertySet.add(visibility)
+    }
+
+    Semantics(properties = propertySet, actions = actions) { children() }
+}
+
+/**
+ * This is a component that emits a semantic node with a single action.
+ *
+ * Since this example does not have node merging implemented, we just creates an semantic action
+ * using the supplied parameters and then invokes the supplied lambda.
+ *
+ * SemanticAction(params) { semanticAction->
+ * ...
+ * }
+ *
+ * For now the [Properties] component accepts a set of actions, but once this is finally
+ * implemented, we will merge the nodes automatically.
+ */
+@Suppress("FunctionName")
+@Composable
+fun <T> SemanticAction(
+    phrase: String = "",
+    defaultParam: T,
+    types: Set<ActionType> = setOf(),
+    action: (ActionParam<T>) -> Unit,
+    @Children block: @Composable() (SemanticAction<T>) -> Unit
+) {
+    val semanticAction = SemanticAction<T>(phrase, defaultParam, types, action)
+    block.invoke(semanticAction)
+}
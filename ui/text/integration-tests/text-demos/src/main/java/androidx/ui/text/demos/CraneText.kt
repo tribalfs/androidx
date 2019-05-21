aosp-mirror/platform_frameworks_support

Use PascalCase for TextDirection
Bug: 126008308
Test: ./gradlew ui-text:test
Test: ./gradlew ui-text:connectedAndroidTest
Change-Id: I48f4afe4cbfbda9bf8f33609d622014abbeede69

ui/text/integration-tests/text-demos/src/main/java/androidx/ui/text/demos/CraneText.kt
@@ -463,7 +463,7 @@ fun TextDemoTextAlign() {
                 )
             }
         }
-        SecondTagLine(tag = "textAlgin = TextAlign.START for LTR")
+        SecondTagLine(tag = "textAlgin = TextAlign.START for Ltr")
         CraneWrapper {
             Text(textAlign = TextAlign.START) {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))
@@ -475,7 +475,7 @@ fun TextDemoTextAlign() {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))
             }
         }
-        SecondTagLine(tag = "textAlgin = TextAlign.END for LTR")
+        SecondTagLine(tag = "textAlgin = TextAlign.END for Ltr")
         CraneWrapper {
             Text(textAlign = TextAlign.END) {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))

tribalfs/androidx

ui/text/integration-tests/text-demos/src/main/java/androidx/ui/text/demos/CraneText.kt
@@ -463,27 +463,27 @@ fun TextDemoTextAlign() {
                 )
             }
         }
-        SecondTagLine(tag = "textAlgin = TextAlign.Start for LTR")
+        SecondTagLine(tag = "textAlgin = TextAlign.Start for Ltr")
         CraneWrapper {
             Text(textAlign = TextAlign.Start) {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))
             }
         }
-        SecondTagLine(tag = "textAlgin = TextAlign.Start for RTL")
+        SecondTagLine(tag = "textAlgin = TextAlign.Start for Rtl")
         CraneWrapper {
-            Text(textDirection = TextDirection.RTL, textAlign = TextAlign.Start) {
+            Text(textDirection = TextDirection.Rtl, textAlign = TextAlign.Start) {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))
             }
         }
-        SecondTagLine(tag = "textAlgin = TextAlign.End for LTR")
+        SecondTagLine(tag = "textAlgin = TextAlign.End for Ltr")
         CraneWrapper {
             Text(textAlign = TextAlign.End) {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))
             }
         }
-        SecondTagLine(tag = "textAlgin = TextAlign.End for RTL")
+        SecondTagLine(tag = "textAlgin = TextAlign.End for Rtl")
         CraneWrapper {
-            Text(textDirection = TextDirection.RTL, textAlign = TextAlign.End) {
+            Text(textDirection = TextDirection.Rtl, textAlign = TextAlign.End) {
                 Span(text = displayText, style = TextStyle(fontSize = fontSize8))
             }
         }
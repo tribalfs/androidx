aosp-mirror/platform_frameworks_support

Convert paging-runtime to .kt
Test: ./gradlew paging:paging-runtime:cC
Change-Id: I84edb1d502967f9c32e2588f7f13ab667c3dd37e

paging/runtime/src/main/java/androidx/paging/LivePagedList.kt
@@ -1,0 +1,105 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging
+
+import androidx.lifecycle.LiveData
+import androidx.paging.futures.FutureCallback
+import androidx.paging.futures.Futures
+
+import com.google.common.util.concurrent.ListenableFuture
+
+import java.util.concurrent.Executor
+
+internal class LivePagedList<Key, Value>(
+    initialKey: Key?,
+    private val config: PagedList.Config,
+    private val boundaryCallback: PagedList.BoundaryCallback<Value>?,
+    private val dataSourceFactory: DataSource.Factory<Key, Value>,
+    private val notifyExecutor: Executor,
+    private val fetchExecutor: Executor
+) : LiveData<PagedList<Value>>(), FutureCallback<PagedList<Value>> {
+    private var currentData: PagedList<Value>
+    private var currentFuture: ListenableFuture<PagedList<Value>>? = null
+
+    private val callback = object : DataSource.InvalidatedCallback {
+        override fun onInvalidated() {
+            invalidate(true)
+        }
+    }
+
+    private val refreshRetryCallback = Runnable { invalidate(true) }
+
+    private val listenableFuture: ListenableFuture<PagedList<Value>>
+        get() {
+            val dataSource = dataSourceFactory.create()
+            currentData.dataSource.removeInvalidatedCallback(callback)
+            dataSource.addInvalidatedCallback(callback)
+            currentData.setInitialLoadState(PagedList.LoadState.LOADING, null)
+
+            @Suppress("UNCHECKED_CAST") // getLastKey guaranteed to be of 'Key' type
+            val lastKey = currentData.lastKey as Key?
+            return PagedList.create(
+                dataSource,
+                notifyExecutor,
+                fetchExecutor,
+                fetchExecutor,
+                boundaryCallback,
+                config,
+                lastKey
+            )
+        }
+
+    override fun onActive() {
+        super.onActive()
+        invalidate(false)
+    }
+
+    init {
+        currentData = InitialPagedList(dataSourceFactory.create(), config, initialKey)
+        onSuccess(currentData)
+    }
+
+    private fun onItemUpdate(previous: PagedList<Value>, next: PagedList<Value>) {
+        previous.setRetryCallback(null)
+        next.setRetryCallback(refreshRetryCallback)
+    }
+
+    override fun onError(throwable: Throwable) {
+        val loadState = if (currentData.dataSource.isRetryableError(throwable)) {
+            PagedList.LoadState.RETRYABLE_ERROR
+        } else {
+            PagedList.LoadState.ERROR
+        }
+
+        currentData.setInitialLoadState(loadState, throwable)
+    }
+
+    override fun onSuccess(value: PagedList<Value>) {
+        onItemUpdate(currentData, value)
+        currentData = value
+        setValue(value)
+    }
+
+    fun invalidate(force: Boolean) {
+        // work is already ongoing, not forcing, so skip invalidate
+        if (currentFuture != null && !force) return
+
+        currentFuture?.cancel(false)
+        currentFuture = listenableFuture
+        Futures.addCallback(currentFuture!!, this, notifyExecutor)
+    }
+}

tribalfs/androidx

paging/runtime/src/main/java/androidx/paging/LivePagedList.kt
@@ -1,0 +1,101 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.paging
+
+import androidx.lifecycle.LiveData
+import androidx.paging.futures.FutureCallback
+import androidx.paging.futures.addCallback
+import com.google.common.util.concurrent.ListenableFuture
+import java.util.concurrent.Executor
+
+internal class LivePagedList<Key : Any, Value : Any>(
+    initialKey: Key?,
+    private val config: PagedList.Config,
+    private val boundaryCallback: PagedList.BoundaryCallback<Value>?,
+    private val dataSourceFactory: DataSource.Factory<Key, Value>,
+    private val notifyExecutor: Executor,
+    private val fetchExecutor: Executor
+) : LiveData<PagedList<Value>>(), FutureCallback<PagedList<Value>> {
+    private var currentData: PagedList<Value>
+    private var currentFuture: ListenableFuture<PagedList<Value>>? = null
+
+    private val callback = object : DataSource.InvalidatedCallback {
+        override fun onInvalidated() {
+            invalidate(true)
+        }
+    }
+
+    private val refreshRetryCallback = Runnable { invalidate(true) }
+
+    init {
+        currentData = InitialPagedList(dataSourceFactory.create(), config, initialKey)
+        onSuccess(currentData)
+    }
+
+    override fun onActive() {
+        super.onActive()
+        invalidate(false)
+    }
+
+    override fun onError(throwable: Throwable) {
+        val loadState = if (currentData.dataSource.isRetryableError(throwable)) {
+            PagedList.LoadState.RETRYABLE_ERROR
+        } else {
+            PagedList.LoadState.ERROR
+        }
+
+        currentData.setInitialLoadState(loadState, throwable)
+    }
+
+    override fun onSuccess(value: PagedList<Value>) {
+        onItemUpdate(currentData, value)
+        currentData = value
+        setValue(value)
+    }
+
+    fun invalidate(force: Boolean) {
+        // work is already ongoing, not forcing, so skip invalidate
+        if (currentFuture != null && !force) return
+
+        currentFuture?.cancel(false)
+        currentFuture = createPagedList().also { it.addCallback(this, notifyExecutor) }
+    }
+
+    private fun onItemUpdate(previous: PagedList<Value>, next: PagedList<Value>) {
+        previous.setRetryCallback(null)
+        next.setRetryCallback(refreshRetryCallback)
+    }
+
+    private fun createPagedList(): ListenableFuture<PagedList<Value>> {
+        val dataSource = dataSourceFactory.create()
+        currentData.dataSource.removeInvalidatedCallback(callback)
+        dataSource.addInvalidatedCallback(callback)
+        currentData.setInitialLoadState(PagedList.LoadState.LOADING, null)
+
+        @Suppress("UNCHECKED_CAST") // getLastKey guaranteed to be of 'Key' type
+        val lastKey = currentData.lastKey as Key?
+        return PagedList.create(
+            dataSource,
+            notifyExecutor,
+            fetchExecutor,
+            fetchExecutor,
+            boundaryCallback,
+            config,
+            lastKey
+        )
+    }
+}
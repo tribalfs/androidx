aosp-mirror/platform_frameworks_support

Add kotlin function types for broken SAM conversions for paging
Two cases here:
 - Public APIs received redundant overloads with kotlin function types
 - Private APIs were simply replaced with kotlin function types

Bug: 134963731
Test: ./gradlew paging:paging-common:check && ./gradlew paging:paging-runtime:cC
Change-Id: Ib4799e9242a9809f8e3ca30c73995ffa8335a0c7

paging/common/src/main/kotlin/androidx/paging/ContiguousPagedList.kt
@@ -229,8 +229,8 @@ open class ContiguousPagedList<K : Any, V : Any>(
         triggerBoundaryCallback(LoadType.REFRESH, initialResult.data)
     }
 
-    override fun dispatchCurrentLoadState(listener: LoadStateListener) =
-        pager.loadStateManager.dispatchCurrentLoadState(listener)
+    override fun dispatchCurrentLoadState(callback: OnLoadStateChanged) =
+        pager.loadStateManager.dispatchCurrentLoadState(callback)
 
     override fun setInitialLoadState(loadState: LoadState, error: Throwable?) =
         pager.loadStateManager.setState(LoadType.REFRESH, loadState, error)

paging/common/src/main/kotlin/androidx/paging/SnapshotPagedList.kt
@@ -40,6 +40,6 @@ internal class SnapshotPagedList<T : Any>(private val pagedList: PagedList<T>) :
 
     override fun detach() {}
     override fun dispatchUpdatesSinceSnapshot(snapshot: PagedList<T>, callback: Callback) {}
-    override fun dispatchCurrentLoadState(listener: LoadStateListener) {}
+    override fun dispatchCurrentLoadState(callback: OnLoadStateChanged) {}
     override fun loadAroundInternal(index: Int) {}
 }

paging/common/src/test/kotlin/androidx/paging/ContiguousPagedListTest.kt
@@ -127,17 +127,11 @@ class ContiguousPagedListTest(private val placeholdersEnabled: Boolean) {
     private fun <E : Any> PagedList<E>.addLoadStateCapture(desiredType: PagedList.LoadType):
             MutableList<PagedList.LoadState> {
         val list = mutableListOf<PagedList.LoadState>()
-        this.addWeakLoadStateListener(object : PagedList.LoadStateListener {
-            override fun onLoadStateChanged(
-                type: PagedList.LoadType,
-                state: PagedList.LoadState,
-                error: Throwable?
-            ) {
-                if (type == desiredType) {
-                    list.add(state)
-                }
+        this.addWeakLoadStateChangedCallback { type, state, _ ->
+            if (type == desiredType) {
+                list.add(state)
             }
-        })
+        }
         return list
     }
 
@@ -650,17 +644,11 @@ class ContiguousPagedListTest(private val placeholdersEnabled: Boolean) {
         // have an error, move loading range, error goes away
         val pagedList = createCountedPagedList(0)
         val states = mutableListOf<PagedList.LoadState>()
-        pagedList.addWeakLoadStateListener(object : PagedList.LoadStateListener {
-            override fun onLoadStateChanged(
-                type: PagedList.LoadType,
-                state: PagedList.LoadState,
-                error: Throwable?
-            ) {
-                if (type == PagedList.LoadType.END) {
-                    states.add(state)
-                }
+        pagedList.addWeakLoadStateChangedCallback { type, state, _ ->
+            if (type == PagedList.LoadType.END) {
+                states.add(state)
             }
-        })
+        }
 
         pagedList.dataSource.enqueueErrorForIndex(45)
         pagedList.loadAround(35)

paging/integration-tests/testapp/src/main/java/androidx/paging/integration/testapp/custom/PagedListSampleActivity.kt
@@ -69,40 +69,35 @@ class PagedListSampleActivity : AppCompatActivity() {
             adapter.currentList?.retry()
         }
 
-        adapter.addLoadStateListener(object : PagedList.LoadStateListener {
-            override fun onLoadStateChanged(
-                type: PagedList.LoadType,
-                state: PagedList.LoadState,
-                error: Throwable?
-            ) {
-                val button = when (type) {
-                    PagedList.LoadType.REFRESH -> buttonRefresh
-                    PagedList.LoadType.START -> buttonStart
-                    PagedList.LoadType.END -> buttonEnd
+        adapter.addLoadStateChangedCallback { type: PagedList.LoadType, state: PagedList.LoadState,
+                                              _: Throwable? ->
+            val button = when (type) {
+                PagedList.LoadType.REFRESH -> buttonRefresh
+                PagedList.LoadType.START -> buttonStart
+                PagedList.LoadType.END -> buttonEnd
+            }
+            when (state) {
+                PagedList.LoadState.IDLE -> {
+                    button.text = "Idle"
+                    button.isEnabled = type == PagedList.LoadType.REFRESH
                 }
-                when (state) {
-                    PagedList.LoadState.IDLE -> {
-                        button.text = "Idle"
-                        button.isEnabled = type == PagedList.LoadType.REFRESH
-                    }
-                    PagedList.LoadState.LOADING -> {
-                        button.text = "Loading"
-                        button.isEnabled = false
-                    }
-                    PagedList.LoadState.DONE -> {
-                        button.text = "Done"
-                        button.isEnabled = false
-                    }
-                    PagedList.LoadState.ERROR -> {
-                        button.text = "Error"
-                        button.isEnabled = false
-                    }
-                    PagedList.LoadState.RETRYABLE_ERROR -> {
-                        button.text = "Error"
-                        button.isEnabled = true
-                    }
+                PagedList.LoadState.LOADING -> {
+                    button.text = "Loading"
+                    button.isEnabled = false
+                }
+                PagedList.LoadState.DONE -> {
+                    button.text = "Done"
+                    button.isEnabled = false
+                }
+                PagedList.LoadState.ERROR -> {
+                    button.text = "Error"
+                    button.isEnabled = false
+                }
+                PagedList.LoadState.RETRYABLE_ERROR -> {
+                    button.text = "Error"
+                    button.isEnabled = true
                 }
             }
-        })
+        }
     }
 }

paging/runtime/src/main/java/androidx/paging/PagedListAdapter.kt
@@ -108,18 +108,13 @@ import androidx.recyclerview.widget.RecyclerView
  */
 abstract class PagedListAdapter<T : Any, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH> {
     internal val differ: AsyncPagedListDiffer<T>
-    private val listener = object : AsyncPagedListDiffer.PagedListListener<T> {
-        override fun onCurrentListChanged(previousList: PagedList<T>?, currentList: PagedList<T>?) {
-            @Suppress("DEPRECATION")
-            this@PagedListAdapter.onCurrentListChanged(currentList)
-            this@PagedListAdapter.onCurrentListChanged(previousList, currentList)
-        }
+    private val listener = { previousList: PagedList<T>?, currentList: PagedList<T>? ->
+        @Suppress("DEPRECATION")
+        this@PagedListAdapter.onCurrentListChanged(currentList)
+        this@PagedListAdapter.onCurrentListChanged(previousList, currentList)
     }
-    private val loadStateListener = object : PagedList.LoadStateListener {
-        override fun onLoadStateChanged(type: LoadType, state: LoadState, error: Throwable?) {
-            this@PagedListAdapter.onLoadStateChanged(type, state, error)
-        }
-    }
+    private val loadStateChangedCallback
+        get() = this::onLoadStateChanged
 
     /**
      * Returns the PagedList currently being displayed by the Adapter.
@@ -148,13 +143,13 @@ abstract class PagedListAdapter<T : Any, VH : RecyclerView.ViewHolder> : Recycle
     protected constructor(diffCallback: DiffUtil.ItemCallback<T>) {
         differ = AsyncPagedListDiffer(this, diffCallback)
         differ.addPagedListListener(listener)
-        differ.addLoadStateListener(loadStateListener)
+        differ.addLoadStateChangedListener(loadStateChangedCallback)
     }
 
     protected constructor(config: AsyncDifferConfig<T>) {
         differ = AsyncPagedListDiffer(AdapterListUpdateCallback(this), config)
         differ.addPagedListListener(listener)
-        differ.addLoadStateListener(loadStateListener)
+        differ.addLoadStateChangedListener(loadStateChangedCallback)
     }
 
     /**
@@ -246,26 +241,26 @@ abstract class PagedListAdapter<T : Any, VH : RecyclerView.ViewHolder> : Recycle
     }
 
     /**
-     * Add a LoadStateListener to observe the loading state of the current PagedList.
+     * Add a [OnLoadStateChanged] to observe the loading state of the current [PagedList].
      *
-     * As new PagedLists are submitted and displayed, the listener will be notified to reflect
+     * As new PagedLists are submitted and displayed, the callback will be notified to reflect
      * current REFRESH, START, and END states.
      *
-     * @param listener Listener to receive updates.
+     * @param callback [OnLoadStateChanged] to receive updates.
      *
-     * @see removeLoadStateListener
+     * @see removeLoadStateChangedCallback
      */
-    open fun addLoadStateListener(listener: PagedList.LoadStateListener) {
-        differ.addLoadStateListener(listener)
+    open fun addLoadStateChangedCallback(callback: OnLoadStateChanged) {
+        differ.addLoadStateChangedListener(callback)
     }
 
     /**
-     * Remove a previously registered LoadStateListener.
+     * Remove a previously registered [OnLoadStateChanged].
      *
-     * @param listener Previously registered listener.
-     * @see addLoadStateListener
+     * @param callback Previously registered callback.
+     * @see addLoadStateChangedCallback
      */
-    open fun removeLoadStateListener(listener: PagedList.LoadStateListener) {
-        differ.removeLoadStateListListener(listener)
+    open fun removeLoadStateChangedCallback(callback: OnLoadStateChanged) {
+        differ.removeLoadStateListListener(callback)
     }
 }

tribalfs/androidx

paging/common/src/main/kotlin/androidx/paging/ContiguousPagedList.kt
@@ -229,8 +229,8 @@ open class ContiguousPagedList<K : Any, V : Any>(
         triggerBoundaryCallback(LoadType.REFRESH, initialResult.data)
     }
 
-    override fun dispatchCurrentLoadState(listener: LoadStateListener) =
-        pager.loadStateManager.dispatchCurrentLoadState(listener)
+    override fun dispatchCurrentLoadState(callback: LoadStateListener) =
+        pager.loadStateManager.dispatchCurrentLoadState(callback)
 
     override fun setInitialLoadState(loadState: LoadState, error: Throwable?) =
         pager.loadStateManager.setState(LoadType.REFRESH, loadState, error)

paging/common/src/main/kotlin/androidx/paging/SnapshotPagedList.kt
@@ -40,6 +40,6 @@ internal class SnapshotPagedList<T : Any>(private val pagedList: PagedList<T>) :
 
     override fun detach() {}
     override fun dispatchUpdatesSinceSnapshot(snapshot: PagedList<T>, callback: Callback) {}
-    override fun dispatchCurrentLoadState(listener: LoadStateListener) {}
+    override fun dispatchCurrentLoadState(callback: LoadStateListener) {}
     override fun loadAroundInternal(index: Int) {}
 }

paging/common/src/test/kotlin/androidx/paging/ContiguousPagedListTest.kt
@@ -127,17 +127,11 @@ class ContiguousPagedListTest(private val placeholdersEnabled: Boolean) {
     private fun <E : Any> PagedList<E>.addLoadStateCapture(desiredType: PagedList.LoadType):
             MutableList<PagedList.LoadState> {
         val list = mutableListOf<PagedList.LoadState>()
-        this.addWeakLoadStateListener(object : PagedList.LoadStateListener {
-            override fun onLoadStateChanged(
-                type: PagedList.LoadType,
-                state: PagedList.LoadState,
-                error: Throwable?
-            ) {
-                if (type == desiredType) {
-                    list.add(state)
-                }
+        this.addWeakLoadStateListener { type, state, _ ->
+            if (type == desiredType) {
+                list.add(state)
             }
-        })
+        }
         return list
     }
 
@@ -650,17 +644,11 @@ class ContiguousPagedListTest(private val placeholdersEnabled: Boolean) {
         // have an error, move loading range, error goes away
         val pagedList = createCountedPagedList(0)
         val states = mutableListOf<PagedList.LoadState>()
-        pagedList.addWeakLoadStateListener(object : PagedList.LoadStateListener {
-            override fun onLoadStateChanged(
-                type: PagedList.LoadType,
-                state: PagedList.LoadState,
-                error: Throwable?
-            ) {
-                if (type == PagedList.LoadType.END) {
-                    states.add(state)
-                }
+        pagedList.addWeakLoadStateListener { type, state, _ ->
+            if (type == PagedList.LoadType.END) {
+                states.add(state)
             }
-        })
+        }
 
         pagedList.dataSource.enqueueErrorForIndex(45)
         pagedList.loadAround(35)

paging/integration-tests/testapp/src/main/java/androidx/paging/integration/testapp/custom/PagedListSampleActivity.kt
@@ -69,40 +69,35 @@ class PagedListSampleActivity : AppCompatActivity() {
             adapter.currentList?.retry()
         }
 
-        adapter.addLoadStateListener(object : PagedList.LoadStateListener {
-            override fun onLoadStateChanged(
-                type: PagedList.LoadType,
-                state: PagedList.LoadState,
-                error: Throwable?
-            ) {
-                val button = when (type) {
-                    PagedList.LoadType.REFRESH -> buttonRefresh
-                    PagedList.LoadType.START -> buttonStart
-                    PagedList.LoadType.END -> buttonEnd
+        adapter.addLoadStateListener { type: PagedList.LoadType, state: PagedList.LoadState,
+                                       _: Throwable? ->
+            val button = when (type) {
+                PagedList.LoadType.REFRESH -> buttonRefresh
+                PagedList.LoadType.START -> buttonStart
+                PagedList.LoadType.END -> buttonEnd
+            }
+            when (state) {
+                PagedList.LoadState.IDLE -> {
+                    button.text = "Idle"
+                    button.isEnabled = type == PagedList.LoadType.REFRESH
                 }
-                when (state) {
-                    PagedList.LoadState.IDLE -> {
-                        button.text = "Idle"
-                        button.isEnabled = type == PagedList.LoadType.REFRESH
-                    }
-                    PagedList.LoadState.LOADING -> {
-                        button.text = "Loading"
-                        button.isEnabled = false
-                    }
-                    PagedList.LoadState.DONE -> {
-                        button.text = "Done"
-                        button.isEnabled = false
-                    }
-                    PagedList.LoadState.ERROR -> {
-                        button.text = "Error"
-                        button.isEnabled = false
-                    }
-                    PagedList.LoadState.RETRYABLE_ERROR -> {
-                        button.text = "Error"
-                        button.isEnabled = true
-                    }
+                PagedList.LoadState.LOADING -> {
+                    button.text = "Loading"
+                    button.isEnabled = false
+                }
+                PagedList.LoadState.DONE -> {
+                    button.text = "Done"
+                    button.isEnabled = false
+                }
+                PagedList.LoadState.ERROR -> {
+                    button.text = "Error"
+                    button.isEnabled = false
+                }
+                PagedList.LoadState.RETRYABLE_ERROR -> {
+                    button.text = "Error"
+                    button.isEnabled = true
                 }
             }
-        })
+        }
     }
 }

paging/runtime/src/main/java/androidx/paging/PagedListAdapter.kt
@@ -108,18 +108,13 @@ import androidx.recyclerview.widget.RecyclerView
  */
 abstract class PagedListAdapter<T : Any, VH : RecyclerView.ViewHolder> : RecyclerView.Adapter<VH> {
     internal val differ: AsyncPagedListDiffer<T>
-    private val listener = object : AsyncPagedListDiffer.PagedListListener<T> {
-        override fun onCurrentListChanged(previousList: PagedList<T>?, currentList: PagedList<T>?) {
-            @Suppress("DEPRECATION")
-            this@PagedListAdapter.onCurrentListChanged(currentList)
-            this@PagedListAdapter.onCurrentListChanged(previousList, currentList)
-        }
+    private val listener = { previousList: PagedList<T>?, currentList: PagedList<T>? ->
+        @Suppress("DEPRECATION")
+        this@PagedListAdapter.onCurrentListChanged(currentList)
+        this@PagedListAdapter.onCurrentListChanged(previousList, currentList)
     }
-    private val loadStateListener = object : PagedList.LoadStateListener {
-        override fun onLoadStateChanged(type: LoadType, state: LoadState, error: Throwable?) {
-            this@PagedListAdapter.onLoadStateChanged(type, state, error)
-        }
-    }
+    private val loadStateListener
+        get() = this::onLoadStateChanged
 
     /**
      * Returns the PagedList currently being displayed by the Adapter.
@@ -246,26 +241,26 @@ abstract class PagedListAdapter<T : Any, VH : RecyclerView.ViewHolder> : Recycle
     }
 
     /**
-     * Add a LoadStateListener to observe the loading state of the current PagedList.
+     * Add a [LoadStateListener] to observe the loading state of the current [PagedList].
      *
-     * As new PagedLists are submitted and displayed, the listener will be notified to reflect
+     * As new PagedLists are submitted and displayed, the callback will be notified to reflect
      * current REFRESH, START, and END states.
      *
-     * @param listener Listener to receive updates.
+     * @param callback [LoadStateListener] to receive updates.
      *
      * @see removeLoadStateListener
      */
-    open fun addLoadStateListener(listener: PagedList.LoadStateListener) {
-        differ.addLoadStateListener(listener)
+    open fun addLoadStateListener(callback: LoadStateListener) {
+        differ.addLoadStateListener(callback)
     }
 
     /**
-     * Remove a previously registered LoadStateListener.
+     * Remove a previously registered [LoadStateListener].
      *
-     * @param listener Previously registered listener.
+     * @param callback Previously registered callback.
      * @see addLoadStateListener
      */
-    open fun removeLoadStateListener(listener: PagedList.LoadStateListener) {
-        differ.removeLoadStateListListener(listener)
+    open fun removeLoadStateListener(callback: LoadStateListener) {
+        differ.removeLoadStateListener(callback)
     }
 }
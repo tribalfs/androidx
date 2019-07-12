aosp-mirror/platform_frameworks_support

Add feature detection for color scheme customization
Test: not needed

Change-Id: Ia0fe5b4b9f170db76caad7a9024ca16db8e7fc24

browser/src/main/java/androidx/browser/customtabs/CustomTabsService.java
@@ -60,6 +60,14 @@ public abstract class CustomTabsService extends Service {
             "androidx.browser.trusted.category.NavBarColorCustomization";
 
     /**
+     * An Intent filter category to signify that the Custom Tabs provider supports selecting and
+     * customizing color schemes via {@link CustomTabsIntent.Builder#setColorScheme} and
+     * {@link CustomTabsIntent.Builder#setColorSchemeParams}.
+     */
+    public static final String CATEGORY_COLOR_SCHEME_CUSTOMIZATION =
+            "androidx.browser.trusted.category.ColorSchemeCustomization";
+
+    /**
      * An Intent filter category to signify that the Custom Tabs provider supports Trusted Web
      * Activities.
      *

tribalfs/androidx

browser/src/main/java/androidx/browser/customtabs/CustomTabsService.java
@@ -56,7 +56,15 @@ public abstract class CustomTabsService extends Service {
      * the color of the navigation bar ({@link CustomTabsIntent.Builder#setNavigationBarColor}).
      */
     public static final String CATEGORY_NAVBAR_COLOR_CUSTOMIZATION =
-            "androidx.browser.trusted.category.NavBarColorCustomization";
+            "androidx.browser.customtabs.category.NavBarColorCustomization";
+
+    /**
+     * An Intent filter category to signify that the Custom Tabs provider supports selecting and
+     * customizing color schemes via {@link CustomTabsIntent.Builder#setColorScheme} and
+     * {@link CustomTabsIntent.Builder#setColorSchemeParams}.
+     */
+    public static final String CATEGORY_COLOR_SCHEME_CUSTOMIZATION =
+            "androidx.browser.customtabs.category.ColorSchemeCustomization";
 
     /**
      * An Intent filter category to signify that the Custom Tabs provider supports Trusted Web
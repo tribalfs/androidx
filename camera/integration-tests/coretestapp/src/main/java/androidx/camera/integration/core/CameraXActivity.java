aosp-mirror/platform_frameworks_support

Retrieve camera id from config instead of only lens-facing.
  Now the camera id is decided by config, including lens-facing and
  customized id.
  clean up camera id query interface.

Bug: b/130538111
Test: ./gradlew camera:camera-core:build, ./gradlew
camera:camera-camera2:build
Change-Id: I960fd00ed24c00d639bc692ef0653c064649c9d0

camera/integration-tests/coretestapp/src/main/java/androidx/camera/integration/core/CameraXActivity.java
@@ -41,7 +41,7 @@ import android.widget.Toast;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraDeviceConfig;
+import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraX;
 import androidx.camera.core.CameraX.LensFacing;
 import androidx.camera.core.FlashMode;
@@ -214,9 +214,8 @@ public class CameraXActivity extends AppCompatActivity
 
     void transformPreview() {
         String cameraId = null;
-        LensFacing previewLensFacing =
-                ((CameraDeviceConfig) mPreview.getUseCaseConfig())
-                        .getLensFacing(/*valueIfMissing=*/ null);
+        PreviewConfig config = (PreviewConfig) mPreview.getUseCaseConfig();
+        LensFacing previewLensFacing = config.getLensFacing(/*valueIfMissing=*/ null);
         if (previewLensFacing != mCurrentCameraLensFacing) {
             throw new IllegalStateException(
                     "Invalid view finder lens facing: "
@@ -225,10 +224,11 @@ public class CameraXActivity extends AppCompatActivity
                             + mCurrentCameraLensFacing);
         }
         try {
-            cameraId = CameraX.getCameraWithLensFacing(previewLensFacing);
-        } catch (Exception e) {
+            cameraId = CameraX.getCameraWithCameraDeviceConfig(config);
+        } catch (CameraInfoUnavailableException e) {
             throw new IllegalArgumentException(
-                    "Unable to get camera id for lens facing " + previewLensFacing, e);
+                    "Unable to get camera id for the camera device config " + config.getLensFacing()
+                            + config.getCameraId(), e);
         }
         Size srcResolution = mPreview.getAttachedSurfaceResolution(cameraId);
 

tribalfs/androidx

  Clean up camera id query interface.

camera/integration-tests/coretestapp/src/main/java/androidx/camera/integration/core/CameraXActivity.java
@@ -42,7 +42,7 @@ import android.widget.Toast;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraDeviceConfig;
+import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraX;
 import androidx.camera.core.CameraX.LensFacing;
 import androidx.camera.core.FlashMode;
@@ -216,9 +216,8 @@ public class CameraXActivity extends AppCompatActivity
 
     void transformPreview() {
         String cameraId = null;
-        LensFacing previewLensFacing =
-                ((CameraDeviceConfig) mPreview.getUseCaseConfig())
-                        .getLensFacing(/*valueIfMissing=*/ null);
+        PreviewConfig config = (PreviewConfig) mPreview.getUseCaseConfig();
+        LensFacing previewLensFacing = config.getLensFacing(/*valueIfMissing=*/ null);
         if (previewLensFacing != mCurrentCameraLensFacing) {
             throw new IllegalStateException(
                     "Invalid preview lens facing: "
@@ -227,10 +226,11 @@ public class CameraXActivity extends AppCompatActivity
                             + mCurrentCameraLensFacing);
         }
         try {
-            cameraId = CameraX.getCameraWithLensFacing(previewLensFacing);
-        } catch (Exception e) {
+            cameraId = CameraX.getCameraWithCameraDeviceConfig(config);
+        } catch (CameraInfoUnavailableException e) {
             throw new IllegalArgumentException(
-                    "Unable to get camera id for lens facing " + previewLensFacing, e);
+                    "Unable to get camera id for the camera device config "
+                            + config.getLensFacing(), e);
         }
         Size srcResolution = mPreview.getAttachedSurfaceResolution(cameraId);
 
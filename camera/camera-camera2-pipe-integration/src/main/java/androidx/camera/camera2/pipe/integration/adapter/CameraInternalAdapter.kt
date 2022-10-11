stitchlv/compose-multiplatform-core

[CameraPipe] Temporarily implement camera release
The CameraInternal#release should be able to wait
for all the output pipeline is finished.
Currently, we didn't have a state of the from
CameraPipe to identify if the camera is closed or
not; so this CL monitors the usage of the output
surface instead of the camera output status.
And complete the release future when all the
Surfaces are inactive.

Bug: 247419208
Test: Parameterized tests for CameraPipe
Change-Id: If6bf154b5d96105d5f001e0ffa06ffeb2bf88ae7

camera/camera-camera2-pipe-integration/src/main/java/androidx/camera/camera2/pipe/integration/adapter/CameraInternalAdapter.kt
@@ -21,20 +21,20 @@ package androidx.camera.camera2.pipe.integration.adapter
 import androidx.annotation.RequiresApi
 import androidx.camera.camera2.pipe.CameraPipe
 import androidx.camera.camera2.pipe.core.Log.debug
-import androidx.camera.camera2.pipe.core.Log.warn
 import androidx.camera.camera2.pipe.integration.config.CameraConfig
 import androidx.camera.camera2.pipe.integration.config.CameraScope
 import androidx.camera.camera2.pipe.integration.impl.UseCaseManager
+import androidx.camera.camera2.pipe.integration.impl.UseCaseThreads
 import androidx.camera.core.UseCase
 import androidx.camera.core.impl.CameraControlInternal
 import androidx.camera.core.impl.CameraInfoInternal
 import androidx.camera.core.impl.CameraInternal
 import androidx.camera.core.impl.LiveDataObservable
 import androidx.camera.core.impl.Observable
-import androidx.camera.core.impl.utils.futures.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import javax.inject.Inject
 import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.launch
 
 internal val cameraAdapterIds = atomic(0)
 
@@ -46,7 +46,8 @@ class CameraInternalAdapter @Inject constructor(
     config: CameraConfig,
     private val useCaseManager: UseCaseManager,
     private val cameraInfo: CameraInfoInternal,
-    private val cameraController: CameraControlInternal
+    private val cameraController: CameraControlInternal,
+    private val threads: UseCaseThreads,
 ) : CameraInternal {
     private val cameraId = config.cameraId
     private val debugId = cameraAdapterIds.incrementAndGet()
@@ -69,9 +70,8 @@ class CameraInternalAdapter @Inject constructor(
     }
 
     override fun release(): ListenableFuture<Void> {
-        warn { "$this#release is not yet implemented." }
-        // TODO: Determine what the correct way to invoke release is.
-        return Futures.immediateFuture(null)
+        // TODO(b/185207100): Implement when CameraState is ready.
+        return threads.scope.launch { useCaseManager.clear() }.asListenableFuture()
     }
 
     override fun getCameraInfoInternal(): CameraInfoInternal = cameraInfo

camera/camera-camera2-pipe-integration/src/main/java/androidx/camera/camera2/pipe/integration/impl/UseCaseManager.kt
@@ -25,6 +25,8 @@ import androidx.camera.camera2.pipe.integration.config.UseCaseCameraConfig
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.UseCase
 import javax.inject.Inject
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.joinAll
 
 /**
  * This class keeps track of the currently attached and active [UseCase]'s for a specific camera.
@@ -70,6 +72,8 @@ class UseCaseManager @Inject constructor(
     private var _activeComponent: UseCaseCameraComponent? = null
     val camera: UseCaseCamera?
         get() = _activeComponent?.getUseCaseCamera()
+
+    private val closingCameraJob = mutableListOf<Job>()
 
     /**
      * This attaches the specified [useCases] to the current set of attached use cases. When any
@@ -162,6 +166,11 @@ class UseCaseManager @Inject constructor(
         }
     }
 
+    suspend fun clear() {
+        detach(attachedUseCases.toList())
+        closingCameraJob.toList().joinAll()
+    }
+
     override fun toString(): String = "UseCaseManager<${cameraConfig.cameraId}>"
 
     private fun refreshRunningUseCases() {
@@ -179,7 +188,12 @@ class UseCaseManager @Inject constructor(
         // Close prior camera graph
         camera.let {
             _activeComponent = null
-            it?.close()
+            it?.close()?.let { closingJob ->
+                closingCameraJob.add(closingJob)
+                closingJob.invokeOnCompletion {
+                    closingCameraJob.remove(closingJob)
+                }
+            }
         }
 
         // Update list of active useCases

tribalfs/androidx

Change-Id: Ia1f0398d5c488b5564c17506b9546be63ebf3cf4

camera/camera-camera2-pipe-integration/src/main/java/androidx/camera/camera2/pipe/integration/adapter/CameraInternalAdapter.kt
@@ -21,20 +21,20 @@ package androidx.camera.camera2.pipe.integration.adapter
 import androidx.annotation.RequiresApi
 import androidx.camera.camera2.pipe.CameraPipe
 import androidx.camera.camera2.pipe.core.Log.debug
-import androidx.camera.camera2.pipe.core.Log.warn
 import androidx.camera.camera2.pipe.integration.config.CameraConfig
 import androidx.camera.camera2.pipe.integration.config.CameraScope
 import androidx.camera.camera2.pipe.integration.impl.UseCaseManager
+import androidx.camera.camera2.pipe.integration.impl.UseCaseThreads
 import androidx.camera.core.UseCase
 import androidx.camera.core.impl.CameraControlInternal
 import androidx.camera.core.impl.CameraInfoInternal
 import androidx.camera.core.impl.CameraInternal
 import androidx.camera.core.impl.LiveDataObservable
 import androidx.camera.core.impl.Observable
-import androidx.camera.core.impl.utils.futures.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import javax.inject.Inject
 import kotlinx.atomicfu.atomic
+import kotlinx.coroutines.launch
 
 internal val cameraAdapterIds = atomic(0)
 
@@ -46,7 +46,8 @@ class CameraInternalAdapter @Inject constructor(
     config: CameraConfig,
     private val useCaseManager: UseCaseManager,
     private val cameraInfo: CameraInfoInternal,
-    private val cameraController: CameraControlInternal
+    private val cameraController: CameraControlInternal,
+    private val threads: UseCaseThreads,
 ) : CameraInternal {
     private val cameraId = config.cameraId
     private val debugId = cameraAdapterIds.incrementAndGet()
@@ -69,9 +70,8 @@ class CameraInternalAdapter @Inject constructor(
     }
 
     override fun release(): ListenableFuture<Void> {
-        warn { "$this#release is not yet implemented." }
-        // TODO: Determine what the correct way to invoke release is.
-        return Futures.immediateFuture(null)
+        // TODO(b/185207100): Implement when CameraState is ready.
+        return threads.scope.launch { useCaseManager.close() }.asListenableFuture()
     }
 
     override fun getCameraInfoInternal(): CameraInfoInternal = cameraInfo

camera/camera-camera2-pipe-integration/src/main/java/androidx/camera/camera2/pipe/integration/impl/UseCaseManager.kt
@@ -26,6 +26,8 @@ import androidx.camera.camera2.pipe.integration.config.UseCaseCameraConfig
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.UseCase
 import javax.inject.Inject
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.joinAll
 
 /**
  * This class keeps track of the currently attached and active [UseCase]'s for a specific camera.
@@ -76,6 +78,8 @@ class UseCaseManager @Inject constructor(
     private var _activeComponent: UseCaseCameraComponent? = null
     val camera: UseCaseCamera?
         get() = _activeComponent?.getUseCaseCamera()
+
+    private val closingCameraJobs = mutableListOf<Job>()
 
     /**
      * This attaches the specified [useCases] to the current set of attached use cases. When any
@@ -176,6 +180,17 @@ class UseCaseManager @Inject constructor(
         }
     }
 
+    suspend fun close() {
+        val closingJobs = synchronized(lock) {
+            if (attachedUseCases.isNotEmpty()) {
+                detach(attachedUseCases.toList())
+            }
+            meteringRepeating.onDetached()
+            closingCameraJobs.toList()
+        }
+        closingJobs.joinAll()
+    }
+
     override fun toString(): String = "UseCaseManager<${cameraConfig.cameraId}>"
 
     @GuardedBy("lock")
@@ -195,7 +210,12 @@ class UseCaseManager @Inject constructor(
         // Close prior camera graph
         camera.let {
             _activeComponent = null
-            it?.close()
+            it?.close()?.let { closingJob ->
+                closingCameraJobs.add(closingJob)
+                closingJob.invokeOnCompletion {
+                    closingCameraJobs.remove(closingJob)
+                }
+            }
         }
 
         // Update list of active useCases